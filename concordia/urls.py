00b5b04cd0c6993c86a60de2387e4f77ab598731
David Krauth <dakrauth@gmail.com>
2018-03-19 12:39:52 -0400

    added basic templates, login, register pages

--- concordia/urls.py
+++ concordia/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     re_path(r'^$', TemplateView.as_view(template_name='home.html')),
-    re_path(r'^accounts/', include('registration.backends.hmac.urls')),
+    re_path(r'^account/', include('registration.backends.hmac.urls')),
+    re_path(r'^account/', include('django.contrib.auth.urls')),
     re_path(r'^wireframes/', include('concordia.experiments.wireframes.urls'))
 ]
