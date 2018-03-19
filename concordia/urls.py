2f1bca84ec705c37544cc3610e40cd3468868afb
David Krauth <dakrauth@gmail.com>
2018-03-19 15:27:23 -0400

    fleshed out skeletal transcribr app

--- concordia/urls.py
+++ concordia/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     re_path(r'^$', TemplateView.as_view(template_name='home.html')),
     re_path(r'^account/', include('registration.backends.hmac.urls')),
     re_path(r'^account/', include('django.contrib.auth.urls')),
-    re_path(r'^wireframes/', include('concordia.experiments.wireframes.urls'))
+    re_path(r'^wireframes/', include('concordia.experiments.wireframes.urls')),
+    re_path(r'^transcribe/', include('concordia.experiments.transcribr.urls')),
 ]
