ec2c946b94cc714cdd3ea0c07db98a0211541f1a
Tim Andrews <tim.andrews@artemisconsultinginc.com>
2018-05-05 00:54:07 -0400

    Added list_display to FAQAdmin

--- faq/admin.py
+++ faq/admin.py
@@ -3,4 +3,13 @@ from .models import FAQ
 
 @admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
-    pass
+    # todo: replace question & answer with truncated values
+    list_display = (
+        'slug',
+        'question',
+        'answer',
+        'created_on',
+        'updated_on',
+        'created_by',
+        'updated_by',
+    )
