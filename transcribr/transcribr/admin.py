0b446d0fab222668808a8e89307414d200a4b830
Tim Andrews <tim.andrews@artemisconsultinginc.com>
2018-05-05 01:47:31 -0400

    Added transcribr admin for Assets, Collections, Subcollections, Tags, Transcriptions & User asset tag collections

--- transcribr/transcribr/admin.py
+++ transcribr/transcribr/admin.py
@@ -19,21 +19,78 @@ class CollectionAdmin(admin.ModelAdmin):
     list_display_links = ('id','title','slug')
 
 
+@admin.register(Subcollection)
+class SubcollectionAdmin(admin.ModelAdmin):
+    # todo: replace metadata with truncated values
+    # todo: add foreignKey link for collection
+    list_display = (
+        'id',
+        'title',
+        'slug',
+        'category',
+        'collection',
+        'metadata',
+        'status',
+    )
+    list_display_links = ('id', 'title', 'slug')
+
+
 @admin.register(Asset)
 class AssetAdmin(admin.ModelAdmin):
-    pass
+    # todo: add truncated values for description & metadata
+    list_display = (
+        'id',
+        'title',
+        'slug',
+        # 'description',
+        'media_url',
+        'media_type',
+        'collection',
+        'subcollection',
+        'sequence',
+        # 'metadata',
+        'status',
+    )
+    list_display_links = ('id', 'title', 'slug')
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'id',
+        'name',
+        'value',
+    )
+    list_display_links = ('id', 'name', 'value')
 
 
 @admin.register(UserAssetTagCollection)
 class UserAssetTagCollectionAdmin(admin.ModelAdmin):
+    # todo: add foreignKey link for asset & user_id
     pass
+    list_display = (
+        'id',
+        'asset',
+        'user_id',
+        'created_on',
+        'updated_on',
+    )
+    list_display_links = ('id', 'asset')
 
 
 @admin.register(Transcription)
 class TranscriptionAdmin(admin.ModelAdmin):
+    # todo: replace text with truncated value
+    # todo: add foreignKey link for asset, parent, & user_id
     pass
+    list_display = (
+        'id',
+        'asset',
+        'parent',
+        'user_id',
+        'text',
+        'status',
+        'created_on',
+        'updated_on',
+    )
+    list_display_links = ('id', 'asset')
