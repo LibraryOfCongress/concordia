348d586eece459a44d5f71ec9a4534b7b1e4f83b
Cameron Eshgh <cameorn.eshgh@artemisconsultinginc.com>
2018-06-22 21:40:47 +0000

    Fixed CSS and JS related to contriubute box, as well as some HTML changes to accommodate those changes—hiding status percentage when transcription not chosen on contribute box.

--- concordia/static/js/contribute.js
+++ concordia/static/js/contribute.js
@@ -1,42 +1,7 @@
-//Make the DIV element draggagle:
-dragElement(document.getElementById(("contribute-box")));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "-tabs")) {
-    /* if present, the header is where you move the DIV from: */
-    document.getElementById(elmnt.id + "-tabs").onmousedown = dragMouseDown;
+$('#nav-pill-transcription, #nav-pill-tag, #nav-pill-discussion').mouseup(function() {
+  if ( !$('#nav-pill-transcription').hasClass('active') ) {
+    $('#status-id,#status-id-label').addClass('d-none');
   } else {
-    /* otherwise, move the DIV from anywhere inside the DIV: */
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    // Need if statemements to keep on screen
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released: */
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+    $('#status-id,#status-id-label').removeClass('d-none');
+    }
+});
