c66375bd2c7986d4266b25df90ce4785975fb908
Cameron Eshgh <cameron.eshgh@artemisconsultinginc.com>
2018-06-26 18:53:28 +0000

    Made the decision to hide save button when viewing asset-related discussion. Also removed verbiage for status label as it's only shown when transcribing task is selected and changed the layout and spacing of the status and save row of items. Finally, also fixed the alignment of the tags input so the cursor didn't default to the bottom of the input box. Fixed a small matter of the textarea for transcription and tagging thinking it was resizable.

--- concordia/static/js/contribute.js
+++ concordia/static/js/contribute.js
@@ -1,7 +1,10 @@
 $('#nav-pill-transcription, #nav-pill-tag, #nav-pill-discussion').click(function( e ) {
   if ( e.target.id == 'nav-pill-transcription' ) {
-    $('#status_id, #status-id-label').removeClass('d-none');
-  } else {
+    $('#status_id,#status-id-label,input.btn-primary').removeClass('d-none');
+  } else if ( e.target.id == 'nav-pill-discussion' ) {
+  	$('#status_id,#status-id-label,input.btn-primary').addClass('d-none');
+  }
+  else {
     $('#status_id,#status-id-label').addClass('d-none');
     }
 });
