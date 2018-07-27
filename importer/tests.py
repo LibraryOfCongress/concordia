700d3f27724936bb78c146895336310241367164
Krishna Kommineni <krishna.kommineni@artemisconsultinginc.com>
2018-07-27 12:17:22 -0400

    Added test cases for importer app

--- importer/tests.py
+++ importer/tests.py
@@ -1,3 +1,118 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from importer.tasks import *
+
+
+class CreateCollectionViewTest(TestCase):
+    """ Test module for CreateCollectionView API """
+
+    def setUp(self):
+        self.valid_item_input = {
+            'name': 'Rosa-Parks-Papers',
+            'url': 'https://www.loc.gov/item/mss859430021',
+            'create_type': 'item'
+        }
+
+        self.valid_collection_input = {
+            'name': 'digital-collection',
+            'url': 'https://www.loc.gov/collections/?fa=partof:law+library+of+congress',
+            'create_type': 'item'
+        }
+
+    def test_create_item_collection(self):
+        client = Client()
+        response = client.post(
+            reverse('create_collection'),
+            data=json.dumps(self.valid_item_input),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_collection(self):
+        client = Client()
+        response = client.post(
+            reverse('create_collection'),
+            data=json.dumps(self.valid_collection_input),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_collection(self):
+        client = Client()
+        response = client.post(
+            reverse('create_collection'),
+            data=json.dumps(self.valid_collection_input),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ImporterTasksTest(TestCase):
+    """Test module for all importer tasks functions"""
+
+    def setUp(self):
+        self.valid_test_data = {
+            'name': 'Rosa-Parks-Papers',
+            'url': 'https://www.loc.gov/item/mss859430021?fo=json',
+            'create_type': 'item',
+        }
+
+        self.valid_collection_test_data = {
+            'name': 'Rosa-Parks-Papers',
+            'url': 'https://www.loc.gov/item/mss859430021?fo=json',
+            'create_type': 'item',
+            'no_of_pages': 1
+        }
+
+        self.invalid_collection_test_data = {
+            'name': 'Rosa-Parks-Papers',
+            'url': 'https://www.loc.gov/item/mss859430021?fo=json',
+            'create_type': 'item',
+            'no_of_pages': 0
+        }
+
+    def test_get_request_data(self):
+        response = get_request_data(self.valid_test_data['url'])
+        self.assertEqual(dict, type(response))
+        self.assertIn(response['resources']['url'], 'https://www.loc.gov/item/mss85943')
+
+    def test_get_collection_pages(self):
+        response = get_collection_pages(self.valid_collection_test_data['url'])
+        self.assertEqual(int, type(response))
+        self.assertIn(response, self.valid_collection_test_data['no_of_pages'])
+
+    def test_get_collection_pages_for_invalid_collection(self):
+        response = get_collection_pages(self.invalid_collection_test_data['url'])
+        self.assertEqual(int, type(response))
+        self.assertIn(response, self.invalid_collection_test_data['no_of_pages'])
+
+    def test_get_collection_params_without_fa(self):
+        curl, cparams = get_collection_params(self.valid_collection_test_data['url'])
+        self.assertEqual(curl, self.valid_collection_test_data['url'])
+        self.assertEqual(cparams, {})
+
+    def test_get_collection_params_with_fa(self):
+        test_url = 'https://www.loc.gov/collections/branch-rickey-papers/?fa=partof:branch+rickey+papers:+baseball+file,+1906-1971'
+        curl, cparams = get_collection_params(test_url)
+        self.assertEqual(curl, test_url)
+        self.assertIn('partof', cparams.get('fa'))
+
+    def test_get_collection_item_ids(self):
+        test_url = 'https://www.loc.gov/collections/branch-rickey-papers/?fa=partof:branch+rickey+papers:+baseball+file,+1906-1971'
+        name = 'branch-rickey-papers'
+        response = get_collection_item_ids(name, test_url)
+        self.assertEqual(list, type(response))
+        self.assertIsNotNone(response)
+
+    def test_get_collection_item_ids_invalid_response(self):
+        test_url = 'https://www.loc.gov/collections/ibbbranch-rickey-papers/?fa=partof:branch+rickey+papers:+baseball+file,+1906-1971'
+        name = 'branch-rickey-papers'
+        response = get_collection_item_ids(name, test_url)
+        self.assertEqual(list, type(response))
+        self.assertEqual(response, [])
