8c8a122e077ec7b18a1f695a176d5088760fe6c2
Rosie Storey <rstorey@loc.gov>
2018-08-02 15:16:47 -0400

    Get latest docker-compose version number for provision script

--- provision_docker_server.sh
+++ provision_docker_server.sh
@@ -13,6 +13,9 @@ sudo add-apt-repository \
 sudo apt-get update -qy
 sudo apt-get install -qy -o Dpkg::Options::='--force-confnew' docker-ce
 
- # Install latest version of docker-compose
- sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
- sudo chmod +x /usr/local/bin/docker-compose
+ # Get latest version of docker-compose
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+# Install docker-compose
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
