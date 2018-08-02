b7fc538c0219eb8aba3acb1bab03f624879ae1c7
Rosie Storey <rstorey@loc.gov>
2018-08-02 13:37:05 -0400

    Add install steps to get latest version of docker-compose

--- provision_docker_server.sh
+++ provision_docker_server.sh
@@ -11,4 +11,8 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update -qy
-sudo apt-get install -qy -o Dpkg::Options::='--force-confnew' docker-ce docker-compose
+sudo apt-get install -qy -o Dpkg::Options::='--force-confnew' docker-ce
+
+ # Install latest version of docker-compose
+ sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+ sudo chmod +x /usr/local/bin/docker-compose
