66014c115226c4a3091fe2e7cd50b8c8cb4f20de
Rosie Storey <rstorey@loc.gov>
2018-08-07 13:48:54 -0400

    Commands for provisioning an AWS app server

--- provision_docker_server.sh
+++ provision_docker_server.sh
@@ -13,9 +13,13 @@ sudo add-apt-repository \
 sudo apt-get update -qy
 sudo apt-get install -qy -o Dpkg::Options::='--force-confnew' docker-ce
 
- # Get latest version of docker-compose
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+sudo sysctl -w vm.max_map_count=262144
 
-# Install docker-compose
-sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+ # Get latest version of docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+git clone https://github.com/LibraryOfCongress/concordia.git
+cd concordia
+cp example_env_file .env
+sudo /usr/local/bin/docker-compose up -d
