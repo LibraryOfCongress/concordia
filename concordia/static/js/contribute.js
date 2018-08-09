6859f9b2fbbfb9d2c0a8e6c09921e7e127e60902
Cameron Eshgh <cameron.eshgh@artemisconsultinginc.com>
2018-08-09 13:39:32 -0400

    Moved alert solution to JS using session storage with sample alert messages.

--- concordia/static/js/contribute.js
+++ concordia/static/js/contribute.js
@@ -2,4 +2,7 @@ $( document ).ready(function() {
   if ( window.location.href.indexOf( "#tab-tag" ) > -1 ) {
     $( '#nav-pill-tag' ).tab( 'show' );
   }
+  $( 'input[type="submit"]' ).click(function() {
+    sessionStorage.setItem('show_message', 'true');
+    });
 });
