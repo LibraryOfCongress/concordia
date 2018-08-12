79dd452c2dd1850d24891356564bef6d198042bd
krishnakommineni <krishna.kommineni@artemisconsultinginc.com>
2018-08-12 13:08:57 +0000

    CHCH-188: fixed typo mistakes and sequence title

--- importer/views.py
+++ importer/views.py
@@ -92,12 +92,15 @@ def check_completeness(ciac, item_id=None):
     return False
 
 
-def save_collection_item_assets(subcollection, the_path):
+def save_collection_item_assets(subcollection, the_path, item_id=None):
 
     for root, dirs, files in os.walk(the_path):
         for filename in files:
             file_path = os.path.join(root, filename)
-            title = file_path.replace(the_path + "/", "").split("/")[0]
+            if item_id:
+                title = item_id
+            else:
+                title = file_path.replace(the_path + "/", "").split("/")[0]
             media_url = file_path.replace(settings.IMPORTER["IMAGES_FOLDER"], "")
             sequence = int(os.path.splitext(filename)[0])
             Asset.objects.create(
@@ -131,12 +134,12 @@ def check_and_save_collection_assets(request, task_id, item_id=None):
         if item_id:
             try:
                 ciac = CollectionItemAssetCount.objects.get(item_task_id=task_id, collection_item_identifier=item_id)
-            except CollectionItemAssetCount.doesnotexist:
+            except CollectionItemAssetCount.DoesNotExist:
                 return Response({"message": "Requested Collection Does not exists"}, status.HTTP_404_NOT_FOUND)
-            if check_and_save_item_completeness(request, ciac):
+            if check_and_save_item_completeness(ciac, item_id):
                 return redirect(
                     reverse(
-                        "transcriptions:project",
+                        "transcriptions:collection",
                         args=[ciac.collection_task.collection_slug, ciac.collection_task.subcollection_slug],
                         current_app=request.resolver_match.namespace,
                     )
@@ -145,12 +148,12 @@ def check_and_save_collection_assets(request, task_id, item_id=None):
             try:
                 ctd = CollectionTaskDetails.objects.get(collection_task_id=task_id)
                 ciac = CollectionItemAssetCount.objects.filter(collection_task=ctd)[0]
-            except CollectionTaskDetails.doesnotexist:
+            except CollectionTaskDetails.DoesNotExist:
                 return Response({"message": "Requested Collection Does not exists"}, status.HTTP_404_NOT_FOUND)
-            if check_and_save_collection_completeness(request, ciac):
+            if check_and_save_collection_completeness(ciac):
                 return redirect(
                     reverse(
-                        "transcriptions:collection",
+                        "transcriptions:project",
                         args=[ctd.collection_slug],
                         current_app=request.resolver_match.namespace,
                     )
@@ -168,7 +171,7 @@ def check_and_save_collection_completeness(ciac):
         try:
             subcollection = Subcollection.objects.get(collection__slug=ciac.collection_task.collection_slug,
                                                    slug=ciac.collection_task.subcollection_slug)
-        except Collection.DoesNotExist:
+        except Subcollection.DoesNotExist:
             collection = Collection.objects.create(title=ciac.collection_task.collection_name,
                                                    slug=ciac.collection_task.collection_slug,
                                                    description=ciac.collection_task.collection_name,
@@ -185,7 +188,7 @@ def check_and_save_collection_completeness(ciac):
         save_collection_item_assets(subcollection, project_local_path)
 
         shutil.rmtree(
-            os.path.join(settings.IMPORTER["IMAGES_FOLDER"], subcollection.collection_slug)
+            os.path.join(settings.IMPORTER["IMAGES_FOLDER"], subcollection.collection.slug, subcollection.slug)
         )
 
         return True
@@ -199,7 +202,7 @@ def check_and_save_item_completeness(ciac, item_id):
         try:
             subcollection = Subcollection.objects.get(collection__slug=ciac.collection_task.collection_slug,
                                                    slug=ciac.collection_task.subcollection_slug)
-        except Collection.DoesNotExist:
+        except Subcollection.DoesNotExist:
             collection = Collection.objects.create(title=ciac.collection_task.collection_name,
                                                    slug=ciac.collection_task.collection_slug,
                                                    description=ciac.collection_task.collection_name,
@@ -209,12 +212,12 @@ def check_and_save_item_completeness(ciac, item_id):
                                          collection=collection,
                                          slug=ciac.collection_task.subcollection_slug)
         item_local_path = os.path.join(
-            settings.IMPORTER["IMAGES_FOLDER"], subcollection.collection.slug, subcollection.sulg, item_id
+            settings.IMPORTER["IMAGES_FOLDER"], subcollection.collection.slug, subcollection.slug, item_id
         )
 
-        save_collection_item_assets(subcollection, item_local_path)
+        save_collection_item_assets(subcollection, item_local_path, item_id)
         shutil.rmtree(
-            os.path.join(settings.IMPORTER["IMAGES_FOLDER"], subcollection.collection.slug, subcollection.sulg)
+            os.path.join(settings.IMPORTER["IMAGES_FOLDER"], subcollection.collection.slug, subcollection.slug)
         )
         return True
 
