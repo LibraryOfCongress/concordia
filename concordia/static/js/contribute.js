1ce8bbe3b4629ab1ea27df955a4a0b5600392012
Cameron Eshgh <cameron.eshgh@artemisconsultinginc.com>
2018-08-14 18:12:32 -0400

    Second pass at making sure contribute message doesn't get displayed again on page refresh.

--- concordia/static/js/contribute.js
+++ concordia/static/js/contribute.js
@@ -2,6 +2,8 @@ $( document ).ready(function() {
   if ( window.location.href.indexOf( "#tab-tag" ) > -1 ) {
     $( '#nav-pill-tag' ).tab( 'show' );
   }
+  sessionStorage.setItem('show_message', 'true');
+
   $( 'input[type="submit"]' ).click(function() {
     sessionStorage.setItem('show_message', 'true');
     });
