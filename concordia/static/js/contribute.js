e793503701dd36e7e0baeb5c68edb052c6a3b5e9
Cameron Eshgh <cameron.eshgh@artemisconsultinginc.com>
2018-08-15 11:03:47 -0400

    Realized a mistake in my logic. The indicator message that wouldn't go away should be addressed by now.

--- concordia/static/js/contribute.js
+++ concordia/static/js/contribute.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
   if ( window.location.href.indexOf( "#tab-tag" ) > -1 ) {
     $( '#nav-pill-tag' ).tab( 'show' );
   }
-  sessionStorage.setItem('show_message', 'false');
 
   $( 'input[type="submit"]' ).click(function() {
     sessionStorage.setItem('show_message', 'true');
