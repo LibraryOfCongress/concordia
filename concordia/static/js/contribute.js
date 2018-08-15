86da5b2a5b7fd0e0dc95827a38fdfa9a89261420
Cameron Eshgh <cameron.eshgh@artemisconsultinginc.com>
2018-08-15 10:56:40 -0400

    Retrying to get confirmation message to not show on page reload

--- concordia/static/js/contribute.js
+++ concordia/static/js/contribute.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   if ( window.location.href.indexOf( "#tab-tag" ) > -1 ) {
     $( '#nav-pill-tag' ).tab( 'show' );
   }
-  sessionStorage.setItem('show_message', 'true');
+  sessionStorage.setItem('show_message', 'false');
 
   $( 'input[type="submit"]' ).click(function() {
     sessionStorage.setItem('show_message', 'true');
