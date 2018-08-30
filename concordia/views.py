86d5b9013476706987b6440afda938cfdf6d9d0f
Cameron Eshgh <cameron.eshgh@artemisconsultinginc.com>
2018-08-30 15:07:36 -0400

    Realized that changing the button text messes up the Django view.

--- concordia/views.py
+++ concordia/views.py
@@ -299,7 +299,7 @@ class ConcordiaAssetView(TemplateView):
         self.get_context_data()
         asset = Asset.objects.get(collection__slug=self.args[0], slug=self.args[1])
 
-        if self.request.POST.get("action").lower() == 'contact manager':
+        if self.request.POST.get("action").lower() == 'contact a manager':
             return redirect(reverse('contact') + "?pre_populate=true")
 
         if self.request.user.is_anonymous:
