11beb67937bbd098fe549130082662641574e57d
Chris Adams <cadams@loc.gov>
2018-09-20 18:41:21 -0400

    Continued importer refactoring
    This still needs the data model to be fixed but it now works
    and restores local storage support which had previously been
    broken but untested.

--- importer/views.py
+++ importer/views.py
@@ -1,8 +1,8 @@
 import os
 import shutil
-import boto3
 from logging import getLogger
 
+import boto3
 from celery.result import AsyncResult
 from django.conf import settings
 from django.shortcuts import redirect
@@ -30,9 +30,11 @@ def get_task_status(request, task_id):
         ciac = CampaignItemAssetCount.objects.get(item_task_id=task_id)
         project_local_path = os.path.join(
             settings.IMPORTER["IMAGES_FOLDER"],
-            ciac.campaign_task.campaign_slug,
-            ciac.campaign_task.project_slug,
+            ciac.campaign_task.project.campaign.slug,
+            ciac.campaign_task.project.slug,
         )
+
+        # FIXME: why don't we trust our records?
         item_downloaded_asset_count = sum(
             [
                 len(files)
@@ -53,7 +55,9 @@ def get_task_status(request, task_id):
         try:
             ctd = CampaignTaskDetails.objects.get(campaign_task_id=task_id)
             project_local_path = os.path.join(
-                settings.IMPORTER["IMAGES_FOLDER"], ctd.campaign_slug, ctd.project_slug
+                settings.IMPORTER["IMAGES_FOLDER"],
+                ctd.project.campaign.slug,
+                ctd.project.slug,
             )
             campaign_downloaded_asset_count = sum(
                 [len(files) for path, dirs, files in os.walk(project_local_path)]
@@ -77,21 +81,20 @@ def check_completeness(ciac, item_id=None):
 
     project_local_path = os.path.join(
         settings.IMPORTER["IMAGES_FOLDER"],
-        ciac.campaign_task.campaign_slug,
-        ciac.campaign_task.project_slug,
+        ciac.campaign_task.project.campaign.slug,
+        ciac.campaign_task.project.slug,
     )
     if item_id:
         item_local_path = os.path.join(project_local_path, item_id)
         item_downloaded_asset_count = sum(
             [len(files) for path, dirs, files in os.walk(item_local_path)]
         )
+
         if ciac.campaign_item_asset_count == item_downloaded_asset_count:
             return True
         else:
+            raise NotImplementedError()
             shutil.rmtree(item_local_path)
-            CampaignTaskDetails.objects.get(
-                campaign_slug=ciac.campaign_task.campaign_slug
-            ).delete()
             return False
 
     else:
@@ -108,9 +111,7 @@ def check_completeness(ciac, item_id=None):
             return True
         else:
             shutil.rmtree(project_local_path)
-            CampaignTaskDetails.objects.get(
-                campaign_slug=ciac.campaign_task.campaign_slug
-            ).delete()
+            raise NotImplementedError()
             return False
     return False
 
@@ -126,29 +127,14 @@ def save_campaign_item_assets(project, the_path, item_id=None):
             else:
                 title = file_path.replace(the_path + "/", "").split("/")[0]
 
-            media_url = file_path.replace(settings.IMPORTER["IMAGES_FOLDER"], "")
+            media_url = os.path.relpath(file_path, settings.IMPORTER["IMAGES_FOLDER"])
             sequence = int(os.path.splitext(filename)[0])
 
-            try:
-                item = Item.objects.get(
-                    campaign__slug=project.campaign,
-                    project__slug=project,
-                    title=title,
-                    slug=title,
-                    item_id=title,
-                )
-            except Item.DoesNotExist:
-                item = Item.objects.create(
-                    campaign=project.campaign,
-                    project=project,
-                    item_id=title,
-                    title=title,
-                    slug=title,
-                )
+            item = project.item_set.get(item_id=title)
 
             asset_info = Asset(
                 title=title,
-                slug="{0}{1}".format(title, sequence),
+                slug="{0}—{1}".format(title, sequence),
                 description="{0} description".format(title),
                 media_url=media_url,
                 media_type="IMG",
@@ -157,21 +143,21 @@ def save_campaign_item_assets(project, the_path, item_id=None):
                 project=project,
                 item=item,
             )
+
             list_asset_info.append(asset_info)
-            try:
-                item_path = "/".join(
-                    os.path.join(settings.MEDIA_ROOT, media_url).split("/")[:-1]
-                )
-                os.makedirs(item_path)
-            except Exception as e:
-                logger.error("Error/warning while creating dir path: %s" % e)
+
+            item_path = "/".join(
+                os.path.join(settings.MEDIA_ROOT, media_url).split("/")[:-1]
+            )
+
+            os.makedirs(item_path, exist_ok=True)
 
     Asset.objects.bulk_create(list_asset_info)
-    if S3_BUCKET_NAME:
-        for a in list_asset_info:
-            source_file_path = os.path.join(
-                settings.IMPORTER["IMAGES_FOLDER"], a.media_url
-            )
+
+    for a in list_asset_info:
+        source_file_path = os.path.join(settings.IMPORTER["IMAGES_FOLDER"], a.media_url)
+
+        if S3_BUCKET_NAME:
             try:
                 S3_CLIENT.upload_file(source_file_path, S3_BUCKET_NAME, a.media_url)
                 logger.info(
@@ -186,14 +172,10 @@ def save_campaign_item_assets(project, the_path, item_id=None):
                     exc_info=True,
                 )
                 raise
-    else:
-        shutil.move(
-            the_path,
-            os.path.join(
-                settings.MEDIA_ROOT,
-                the_path.replace(settings.IMPORTER["IMAGES_FOLDER"], ""),
-            ),
-        )
+        else:
+            shutil.move(
+                source_file_path, os.path.join(settings.MEDIA_ROOT, a.media_url)
+            )
 
 
 @api_view(http_method_names=["GET"])
@@ -216,8 +198,8 @@ def check_and_save_campaign_assets(request, task_id, item_id=None):
                 reverse(
                     "transcriptions:project-detail",
                     args=[
-                        ciac.campaign_task.campaign_slug,
-                        ciac.campaign_task.project_slug,
+                        ciac.campaign_task.project.campaign.slug,
+                        ciac.campaign_task.project.slug,
                     ],
                     current_app=request.resolver_match.namespace,
                 )
@@ -255,7 +237,7 @@ def check_and_save_campaign_completeness(ciac):
         try:
             project = Project.objects.get(
                 campaign__slug=ciac.campaign_task.campaign_slug,
-                slug=ciac.campaign_task.project_slug,
+                slug=ciac.campaign_task.project.slug,
             )
         except Project.DoesNotExist:
             if S3_BUCKET_NAME:
@@ -273,7 +255,7 @@ def check_and_save_campaign_completeness(ciac):
             project = Project.objects.create(
                 title=ciac.campaign_task.project_name,
                 campaign=campaign,
-                slug=ciac.campaign_task.project_slug,
+                slug=ciac.campaign_task.project.slug,
             )
 
         project_local_path = os.path.join(
@@ -288,58 +270,6 @@ def check_and_save_campaign_completeness(ciac):
             )
         )
 
-        return True
-
-    return False
-
-
-def check_and_save_item_completeness(ciac, item_id):
-
-    if check_completeness(ciac, item_id):
-        if S3_BUCKET_NAME:
-            s3_storage = True
-        else:
-            s3_storage = False
-        campaign, created = Campaign.objects.get_or_create(
-            title=ciac.campaign_task.campaign_name,
-            slug=ciac.campaign_task.campaign_slug,
-            description=ciac.campaign_task.campaign_name,
-            is_active=True,
-            s3_storage=s3_storage,
-        )
-
-        try:
-            project = Project.objects.get(
-                campaign__slug=ciac.campaign_task.campaign_slug,
-                slug=ciac.campaign_task.project_slug,
-            )
-        except Project.DoesNotExist:
-
-            project = Project.objects.create(
-                title=ciac.campaign_task.project_name,
-                campaign=campaign,
-                slug=ciac.campaign_task.project_slug,
-            )
-
-        item, created = Item.objects.get_or_create(
-            project=ciac.campaign_task.project,
-            item_id=item_id,
-            defaults={"title": item_id, "slug": item_id},
-        )
-
-        item_local_path = os.path.join(
-            settings.IMPORTER["IMAGES_FOLDER"],
-            project.campaign.slug,
-            project.slug,
-            item_id,
-        )
-
-        save_campaign_item_assets(project, item_local_path, item_id)
-        shutil.rmtree(
-            os.path.join(
-                settings.IMPORTER["IMAGES_FOLDER"], project.campaign.slug, project.slug
-            )
-        )
         return True
 
     return False
