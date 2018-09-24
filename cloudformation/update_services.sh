efb2cfbbb79fc3a2723b6d28e601ae0326f73b26
Rosie Storey <rstorey@loc.gov>
2018-09-24 10:34:08 -0400

    Add deploy steps to cloudformation README

--- cloudformation/update_services.sh
+++ cloudformation/update_services.sh
@@ -7,6 +7,4 @@ AWS_REGION=us-east-1
 
 export CLUSTER_NAME AWS_REGION
 
-aws ecs register-task-definition --cli-input-json file://task_definitions.json
-
 aws ecs update-service --cluster $CLUSTER_NAME --service concordia --force-new-deployment
