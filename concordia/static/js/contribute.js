7e6b95b73dbedc504bb9f59e57b801c7c2c0a49d
Chris Adams <cadams@loc.gov>
2018-09-27 12:47:27 -0400

    Basic transcription submission support
    This implements the core logic where the user can save their
    work or mark the transcription as ready for review. 
    
    In the edit state, a message will be displayed and they may 
    continue to work. 
    
    In the submitted state, a message will be displayed 
    suggesting that they tag the page before continuing; the
    edit controls are locked to prevent modification after the
    page has been marked as ready for review.

--- concordia/static/js/contribute.js
+++ concordia/static/js/contribute.js
@@ -1,32 +1,112 @@
-$(document).ready(function() {
-    if (window.location.href.indexOf('#tab-tag') > -1) {
-        $('#nav-pill-tag').tab('show');
-        $('#tag-input').focus();
-    }
+/* global $ displayMessage */
 
-    $('#nav-pill-tag, #nav-pill-transcription').click(function(e) {
-        var target = e.target.id;
-        changeTheURL(target);
+function lockControls($container) {
+    // Locks all of the controls in the provided jQuery element
+    $container.find('input, textarea').attr('readonly', 'readonly');
+    $container.find('button').attr('disabled', 'disabled');
+}
+
+function unlockControls($container) {
+    // Locks all of the controls in the provided jQuery element
+    $container.find('input, textarea').removeAttr('readonly');
+    $container.find('button').removeAttr('disabled');
+}
+
+$('form.ajax-submission').each(function(idx, formElement) {
+    /*
+    Generic AJAX submission logic which takes a form and POSTs its data to the
+    configured action URL, locking the controls until it gets a response either
+    way.
+
+    If the AJAX request is successful, the form-submit-success custom event will
+    be triggered. On failures, form-submit-failure will be triggered after
+    unlocking the controls.
+
+    Because there's no standard way to get the value of the submit button
+    clicked, and forms may be submitted without using a button at all, the
+    <form> element may have optional data-submit-name and data-submit-value
+    attributes for the default values and a click handler will be used to
+    update those values based on user interaction.
+    */
+
+    var $form = $(formElement);
+
+    $form.on('click', '[type=submit]', function(evt) {
+        $form.data('submit-name', evt.target.name);
+        $form.data('submit-value', evt.target.value);
     });
 
-    $('input[type="submit"]').click(function(e) {
-        var target = e.target.id;
-        sessionStorage.setItem('show_message', 'true');
+    $form.on('submit', function(evt) {
+        evt.preventDefault();
 
-        if (target == 'save-button') {
-            sessionStorage.setItem('status', 'save');
-        } else if (target == 'review-button') {
-            sessionStorage.setItem('status', 'review');
-        } else if (target == 'complete-button') {
-            sessionStorage.setItem('status', 'complete');
+        lockControls($form);
+
+        var formData = $form.serializeArray();
+
+        var submitName = $form.data('submit-name'),
+            submitValue = $form.data('submit-value');
+        if (submitName) {
+            formData.push({name: submitName, value: submitValue});
         }
+
+        $.ajax({
+            url: $form.attr('action'),
+            method: 'POST',
+            data: $.param(formData)
+        })
+            .done(function(data, textStatus) {
+                $form.trigger('form-submit-success', {
+                    textStatus: textStatus,
+                    requestData: formData,
+                    responseData: data,
+                    $form: $form
+                });
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                unlockControls($form);
+
+                $form.trigger('form-submit-failure', {
+                    textStatus: textStatus,
+                    errorThrown: errorThrown,
+                    requestData: formData,
+                    $form: $form
+                });
+            });
+
+        return false;
     });
 });
 
-function changeTheURL(nav) {
-    if (nav == 'nav-pill-tag') {
-        window.location.hash = 'tab-tag';
-    } else {
-        window.location.hash = 'tab-transcription';
-    }
-}
+$('#transcription-editor')
+    .on('form-submit-success', function(evt, extra) {
+        var status = extra.responseData.status;
+        if (status == 'Edit') {
+            unlockControls(extra.$form);
+            displayMessage(
+                'info',
+                "Successfully saved your work. Submit it for review when you're done",
+                'transcription-save-result'
+            );
+        } else if (status == 'Submitted') {
+            displayMessage(
+                'info',
+                'Successfully submitted your work for review. After you are done tagging, go to the next page',
+                'transcription-save-result'
+            );
+        } else {
+            displayMessage(
+                'info',
+                'Submit successful. Implement the next stage for ' +
+                    status +
+                    '!',
+                'transcription-save-result'
+            );
+        }
+    })
+    .on('form-submit-failure', function(evt, info) {
+        displayMessage(
+            'error',
+            'Unable to save your work: ' + info.textStatus + info.errorThrown,
+            'transcription-save-result'
+        );
+    });
