72d08c13bc54a18c0c632e4d8a189c23ac6c0d11
Chris Adams <cadams@loc.gov>
2018-10-03 19:42:19 -0400

    Upgrade minimum version of Django to 2.0.9
    This avoids security regressions if installed in a manner
    which somehow ignored the newer 2.0.x releases.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 VERSION = __import__("concordia").get_version()
-INSTALL_REQUIREMENTS = ["Django<2.1"]
+INSTALL_REQUIREMENTS = ["<2.1,>=2.0.9"]
 SCRIPTS = ["manage.py"]
 DESCRIPTION = "Transcription crowdsourcing"
 CLASSIFIERS = """\
