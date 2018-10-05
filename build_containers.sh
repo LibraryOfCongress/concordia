e3c2321e2f9bfd1b0629912352952eb3153d8d8b
Rosie Storey <rstorey@loc.gov>
2018-10-05 11:01:11 -0400

    Try to get jenkins to use venv and tag docker containers with version number

--- build_containers.sh
+++ build_containers.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-set -eu
+
+set -euox pipefail
+
+# Get an unique venv folder to using *inside* workspace
+VENV=".venv-$BUILD_NUMBER"
+
+# Initialize new venv
+virtualenv "$VENV"
+
+# Update pip
+source "$VENV/bin/activate"
+
+pip3 install pipenv
+pipenv install --dev --deploy
 
 AWS_ACCOUNT_ID="$(aws sts get-caller-identity  --output=text --query "Account")"
+
+FULL_VERSION_NUMBER="$(python3 setup.py --version)"
+
+VERSION_NUMBER=$(echo $FULL_VERSION_NUMBER| cut -d '+' -f 1)
 
 eval "$(aws ecr get-login --no-include-email --region us-east-1)"
 
 docker build -t concordia .
-docker tag concordia:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia:latest
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia:latest
+docker tag concordia:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia:${VERSION_NUMBER}
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia:${VERSION_NUMBER}
 
 docker build -t concordia/importer:latest --file importer/Dockerfile .
-docker tag concordia/importer:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/importer:latest
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/importer:latest
+docker tag concordia/importer:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/importer:${VERSION_NUMBER}
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/importer:${VERSION_NUMBER}
 
-# docker pull rabbitmq:latest
-# docker tag rabbitmq:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/rabbitmq:latest
-# docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/rabbitmq:latest
-
-# aws ecs update-service --cluster concordia2 --service concordia-service --force-new-deployment
+docker pull rabbitmq:latest
+docker tag rabbitmq:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/rabbitmq:${VERSION_NUMBER}
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/rabbitmq:${VERSION_NUMBER}
