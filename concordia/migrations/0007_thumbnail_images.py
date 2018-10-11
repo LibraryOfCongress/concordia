6006b5964fa9a21a8195826735a72be0628138dc
Rosie Storey <rstorey@loc.gov>
2018-10-10 22:05:41 -0400

    Fix typo in migration dependency name

--- concordia/migrations/0007_thumbnail_images.py
+++ concordia/migrations/0007_thumbnail_images.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [("concordia", "0006_campaign_resource")]
+    dependencies = [("concordia", "0006_campaignresource")]
 
     operations = [
         migrations.AddField(
