d65247e6bdba1a5827142eb81824cedf1ae016f1
Chris Adams <chris@improbable.org>
2018-10-18 11:09:59 -0400

    Update concordia/views.py

--- concordia/views.py
+++ concordia/views.py
@@ -403,7 +403,7 @@ def save_transcription(request, *, asset_pk):
     # Check whether this transcription text contains any URLs
     # If so, ask the user to correct the transcription by removing the URLs
     transcription_text = request.POST["text"]
-    urls = re.search(URL_REGEX, transcription_text)
+    url_match = re.search(URL_REGEX, transcription_text)
     if urls:
         return JsonResponse(
             {
