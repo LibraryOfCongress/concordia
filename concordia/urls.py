853ef0149cbb9fcfff2aaf9770544852a1983a45
Chris Adams <cadams@loc.gov>
2018-10-18 13:44:38 -0400

    Remove duplicate view declaration
    The auth related views were declared twice; the second copy
    was never used since the full include always matched first.

--- concordia/urls.py
+++ concordia/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.http import Http404, HttpResponseForbidden
 from django.urls import include, path, re_path
 from django.views.defaults import page_not_found, permission_denied, server_error
@@ -121,22 +120,6 @@ urlpatterns = [
     path("forum/", include(board.urls)),
     path("captcha/ajax/", views.ajax_captcha, name="ajax-captcha"),
     path("captcha/", include("captcha.urls")),
-    re_path(r"^password_reset/$", auth_views.password_reset, name="password_reset"),
-    re_path(
-        r"^password_reset/done/$",
-        auth_views.password_reset_done,
-        name="password_reset_done",
-    ),
-    re_path(
-        r"^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
-        auth_views.password_reset_confirm,
-        name="password_reset_confirm",
-    ),
-    re_path(
-        r"^reset/done/$",
-        auth_views.password_reset_complete,
-        name="password_reset_complete",
-    ),
     # Internal support assists:
     path("maintenance-mode/", include("maintenance_mode.urls")),
     path("error/500/", server_error),
