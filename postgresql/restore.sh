038c4255cb9b7ba83c62a72139a46ebdc31a7b9e
Rosie Storey <rstorey@loc.gov>
2018-10-31 16:04:37 -0400

    Add info how to install 9.6 version of postgresql client

--- postgresql/restore.sh
+++ postgresql/restore.sh
@@ -1,10 +1,18 @@
 
 #!/bin/bash
 
+# For AMI: sudo yum install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-6-x86_64/pgdg-ami201503-96-9.6-2.noarch.rpm
+#          sudo yum install postgresql96
+
 # Before running the restore, you'll have to stop the ECS task to close any open connections.
 # Then, run the following to drop the database.
 # psql -U concordia -h $POSTGRES_HOST
 # \c postgres
 # drop database concordia
 
-pg_restore --create --clean -U concordia -h $POSTGRES_HOST -Fc -n --dbname=postgres --no-owner --no-acl $DUMP_FILE
+export POSTGRES_HOST=cpl1jbd411xj1t.cbvco2dnalbp.us-east-1.rds.amazonaws.com
+export DUMP_FILE=concordia.dmp
+
+pg_restore --create --clean -U concordia -h $POSTGRES_HOST -Fc -n --dbname=postgres --no-owner --no-acl -f $DUMP_FILE
+
+# After this, change the Sites in django admin to match the host name
