e7b5ec2cf1f76b4607c5c83357d0077adbaffbe4
Rosie Storey <rstorey@loc.gov>
2018-10-31 16:21:47 -0400

    add required semicolon to drop database statement

--- postgresql/restore.sh
+++ postgresql/restore.sh
@@ -1,16 +1,16 @@
 
 #!/bin/bash
 
-# For AMI: sudo yum install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-6-x86_64/pgdg-ami201503-96-9.6-2.noarch.rpm
-#          sudo yum install postgresql96
+# For AMI: sudo yum -y install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-6-x86_64/pgdg-ami201503-96-9.6-2.noarch.rpm
+#          sudo yum -y install postgresql96
 
 # Before running the restore, you'll have to stop the ECS task to close any open connections.
 # Then, run the following to drop the database.
 # psql -U concordia -h $POSTGRES_HOST
 # \c postgres
-# drop database concordia
+# drop database concordia;
 
-export POSTGRES_HOST=cpl1jbd411xj1t.cbvco2dnalbp.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=
 export DUMP_FILE=concordia.dmp
 
 pg_restore --create --clean -U concordia -h $POSTGRES_HOST -Fc --dbname=postgres --no-owner --no-acl $DUMP_FILE
