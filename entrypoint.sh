f9704a92608bdb8f0049df27d5882e3dc4a3d738
Chris Adams <cadams@loc.gov>
2018-11-07 14:13:28 -0500

    Automation-friendly database configuration
    This command makes it safe to have a deployment step which runs every
    time we launch a container to ensure that the database has the expected
    admin user and site configuration.
    
    Closes #392

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,9 @@ echo Running makemigrations
 echo Running migrations
 ./manage.py migrate
 
+echo "Ensuring our base configuration is present in the database"
+./manage.py ensure_initial_site_configuration
+
 echo Running collectstatic
 ./manage.py collectstatic --clear --noinput -v0
 
