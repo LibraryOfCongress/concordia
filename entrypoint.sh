6fd2a0e812fc4616f739f79fd352ac5af0e466e6
Chris Adams <cadams@loc.gov>
2018-11-09 11:00:05 -0500

    Call `raven test` on startup
    This ensures that starting a container will trigger an error if the Sentry configuration is incorrect

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,9 @@ echo Running migrations
 echo "Ensuring our base configuration is present in the database"
 ./manage.py ensure_initial_site_configuration
 
+echo "Testing Sentry configuration"
+./manage.py raven test
+
 echo Running collectstatic
 ./manage.py collectstatic --clear --noinput -v0
 
