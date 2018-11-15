54563cfe6208312bbfa0f18bb0c4d6c14b1a501d
Chris Adams <cadams@loc.gov>
2018-11-15 15:05:41 -0500

    Remove asset counts from filter buttons
    This saves a fair amount of screen real-estate

--- concordia/templatetags/concordia_filtering_tags.py
+++ concordia/templatetags/concordia_filtering_tags.py
@@ -11,20 +11,16 @@ register = template.Library()
 def transcription_status_filters(status_counts, active_value):
     ctx = {}
 
-    status_count_map = {key: count for key, label, count in status_counts}
-    total_count = sum(status_count_map.values())
-
     ctx["status_choices"] = status_choices = [
-        ("", "active" if not active_value else "", f"All ({total_count})")
+        ("", "active" if not active_value else "", f"All")
     ]
 
     for key, label in TranscriptionStatus.CHOICES:
-        asset_count = status_count_map.get(key, 0)
         status_choices.append(
             (
                 "?transcription_status=%s" % urlquote(key),
                 "active" if active_value == key else "",
-                f"{label} ({asset_count})",
+                label,
             )
         )
 
