9da05a86a2fee0f83faae719b6a7ad79b9de13d1
Rosie Storey <rstorey@loc.gov>
2018-11-16 11:24:46 -0500

    Touch log files for indexer docker container

--- indexer/entrypoint.sh
+++ indexer/entrypoint.sh
@@ -2,5 +2,11 @@
 
 set -e -u # Exit immediately for unhandled errors or undefined variables
 
+
+mkdir -p /app/logs
+touch /app/logs/concordia.log
+touch /app/logs/concordia-celery.log
+
+
 echo Running indexing
 ./manage.py search_index --rebuild -f
