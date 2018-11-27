0571edf2ec1fbc50637c9991da67b106f3eb4dce
Chris Adams <cadams@loc.gov>
2018-11-27 14:25:57 -0500

    Simple Page: basic __str__
    This makes the pages nicer to see in the admin navigation

--- concordia/models.py
+++ concordia/models.py
@@ -324,3 +324,6 @@ class SimplePage(models.Model):
     title = models.CharField(max_length=200)
 
     body = models.TextField()
+
+    def __str__(self):
+        return f"SimplePage: {self.path}"
