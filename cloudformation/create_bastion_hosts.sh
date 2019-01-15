267765833cfbe9e2212479841331b1052321b155
Rosie Storey <rstorey@loc.gov>
2019-01-15 16:31:59 -0500

    Allow Jenkins to set ENV_NAME for creating bastion hosts

--- cloudformation/create_bastion_hosts.sh
+++ cloudformation/create_bastion_hosts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export ENV_NAME=dev
+export ENV_NAME=${ENV_NAME:-dev}
 
 aws cloudformation create-stack --stack-name "${ENV_NAME}-bastionhosts"  --template-url https://s3.amazonaws.com/crowd-deployment/infrastructure/bastion-hosts.yaml --parameters "ParameterKey=EnvironmentName,ParameterValue=${ENV_NAME}" ParameterKey=KeyPairName,ParameterValue=rstorey@loc.gov
