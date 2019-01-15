21f8b4b0440120ffe9c68efffc065e54e39e7920
Rosie Storey <rstorey@loc.gov>
2019-01-15 15:00:17 -0500

    Use mapping in bastion host template to simplify parameters

---
+++ cloudformation/create_bastion_hosts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export ENV_NAME=dev
+
+aws cloudformation create-stack --stack-name "${ENV_NAME}-bastionhosts"  --template-url https://s3.amazonaws.com/crowd-deployment/infrastructure/bastion-hosts.yaml --parameters "ParameterKey=EnvironmentName,ParameterValue=${ENV_NAME}" ParameterKey=KeyPairName,ParameterValue=rstorey@loc.gov
