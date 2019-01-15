66cd5bf5fd23297a85580f54cc67dad3c55c716a
Rosie Storey <rstorey@loc.gov>
2019-01-15 09:15:02 -0500

    Increase django version in setup.py files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 VERSION = __import__("concordia").get_version()
-INSTALL_REQUIREMENTS = ["boto3", "Django<2.1,>=2.0.9"]
+INSTALL_REQUIREMENTS = ["boto3", "Django>=2.1.5"]
 SCRIPTS = ["manage.py"]
 DESCRIPTION = "Transcription crowdsourcing"
 CLASSIFIERS = """\
