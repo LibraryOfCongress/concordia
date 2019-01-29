223c004d77f1c304def625d57a34f53d50458e27
Rosie Storey <rstorey@loc.gov>
2019-01-29 17:12:06 -0500

    Get password out of secret storage; ovewrite .pgpass file every time

--- db_scripts/dump.sh
+++ db_scripts/dump.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# export ENV_NAME=prod
+export ENV_NAME=prod
+
 # aws cloudformation create-stack --region us-east-1 --stack-name $ENV_NAME-bastionhosts --template-url https://s3.amazonaws.com/crowd-deployment/infrastructure/bastion-hosts.yaml --parameters ParameterKey=EnvironmentName,ParameterValue=$ENV_NAME ParameterKey=KeyPairName,ParameterValue=rstorey@loc.gov --disable-rollback
 # aws cloudformation delete-stack --region us-east-1 --stack-name $ENV_NAME-bastionhosts
 
-export TODAY=20190129
-export POSTGRESQL_PW=${POSTGRESQL_PW:-password}
+export TODAY=$(date +%Y%m%d)
+export POSTGRESQL_PW="$(aws secretsmanager get-secret-value --secret-id crowd/${ENV_NAME}/DB/MasterUserPassword | python3 -c 'import json,sys;Secret=json.load(sys.stdin);SecretString=json.loads(Secret["SecretString"]);print(SecretString["password"])')"
 export POSTGRESQL_HOST=${POSTGRESQL_HOST:-localhost}
 export DUMP_FILE=concordia.dmp
 
-echo "$POSTGRESQL_HOST:5432:*:concordia:$POSTGRESQL_PW" >> ~/.pgpass
+echo "${POSTGRESQL_HOST}:5432:*:concordia:${POSTGRESQL_PW}" > ~/.pgpass
 chmod 600 ~/.pgpass
 
-pg_dump -Fc --clean --create --no-owner --no-acl -U concordia -h "$POSTGRESQL_HOST" concordia -f "$DUMP_FILE"
-aws s3 cp "$DUMP_FILE" s3://crowd-deployment/database-dumps/concordia.$TODAY.dmp
-aws s3 cp "$DUMP_FILE" s3://crowd-deployment/database-dumps/concordia.latest.dmp
+pg_dump -Fc --clean --create --no-owner --no-acl -U concordia -h "${POSTGRESQL_HOST}" concordia -f "${DUMP_FILE}"
+aws s3 cp "${DUMP_FILE}" "s3://crowd-deployment/database-dumps/concordia.${TODAY}.dmp"
+aws s3 cp "${DUMP_FILE}" s3://crowd-deployment/database-dumps/concordia.latest.dmp
--- db_scripts/restore.sh
+++ db_scripts/restore.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# export ENV_NAME=dev
+
+export ENV_NAME=dev
+
 # aws cloudformation create-stack --region us-east-1 --stack-name $ENV_NAME-bastionhosts --template-url https://s3.amazonaws.com/crowd-deployment/infrastructure/bastion-hosts.yaml --parameters ParameterKey=EnvironmentName,ParameterValue=$ENV_NAME ParameterKey=KeyPairName,ParameterValue=rstorey@loc.gov --disable-rollback
 # aws cloudformation delete-stack --region us-east-1 --stack-name $ENV_NAME-bastionhosts
 
-export POSTGRESQL_PW=${POSTGRESQL_PW:-password}
+export POSTGRESQL_PW="$(aws secretsmanager get-secret-value --secret-id crowd/${ENV_NAME}/DB/MasterUserPassword | python3 -c 'import json,sys;Secret=json.load(sys.stdin);SecretString=json.loads(Secret["SecretString"]);print(SecretString["password"])')"
 export POSTGRESQL_HOST=${POSTGRESQL_HOST:-localhost}
 export DUMP_FILE=/concordia.dmp
 
-echo "$POSTGRESQL_HOST:5432:*:concordia:$POSTGRESQL_PW" >> ~/.pgpass
+echo "$POSTGRESQL_HOST:5432:*:concordia:$POSTGRESQL_PW" > ~/.pgpass
 chmod 600 ~/.pgpass
 
 psql -U concordia -h "$POSTGRESQL_HOST" -d postgres -c "select pg_terminate_backend(pid) from pg_stat_activity where datname='concordia';"
