521faf77ea3aa437de30183393a79dc97823fb65
Rosie Storey <rstorey@loc.gov>
2019-01-31 10:22:30 -0500

    Rename PUSH to PUBLISH_CONTAINERS for clarity

--- build_containers.sh
+++ build_containers.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail
 BUILD_ALL=${BUILD_ALL:=0}
 BUILD_NUMBER=${BUILD_NUMBER:=1}
 TAG=${TAG:-test}
-PUSH=${PUSH:=1}
+PUBLISH_CONTAINERS=${PUBLISH_CONTAINERS:=1}
 
 # Get an unique venv folder to use inside workspace
 VENV=".venv-${BUILD_NUMBER}"
@@ -22,7 +22,7 @@ pipenv install --dev --deploy
 FULL_VERSION_NUMBER="$(python3 setup.py --version)"
 VERSION_NUMBER=$(echo "${FULL_VERSION_NUMBER}" | cut -d '+' -f 1)
 
-if [ $PUSH -eq 1 ]; then
+if [ $PUBLISH_CONTAINERS -eq 1 ]; then
     AWS_ACCOUNT_ID="$(aws sts get-caller-identity  --output=text --query "Account")"
     eval "$(aws ecr get-login --no-include-email --region us-east-1)"
 fi
@@ -31,7 +31,7 @@ python3 setup.py build
 
 docker build -t concordia .
 
-if [ $PUSH -eq 1 ]; then
+if [ $PUBLISH_CONTAINERS -eq 1 ]; then
     docker tag concordia:latest "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia:${VERSION_NUMBER}"
     docker tag concordia:latest "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia:${TAG}"
     docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia:${VERSION_NUMBER}"
@@ -44,7 +44,7 @@ if [ $BUILD_ALL -eq 1 ]; then
     docker build -t concordia/celerybeat --file celerybeat/Dockerfile .
     docker build -t concordia/indexer --file indexer/Dockerfile .
 
-    if [ $PUSH -eq 1 ]; then
+    if [ $PUBLISH_CONTAINERS -eq 1 ]; then
         docker tag concordia/importer:latest "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/importer:${VERSION_NUMBER}"
         docker tag concordia/importer:latest "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/importer:${TAG}"
         docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/importer:${VERSION_NUMBER}"
