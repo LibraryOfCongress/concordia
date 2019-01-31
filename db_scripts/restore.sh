cc71a3f3b11632ed947215aad1f7a09d836cf849
Rosie Storey <rstorey@loc.gov>
2019-01-31 11:31:26 -0500

    refactoring of db scripts

--- db_scripts/restore.sh
+++ db_scripts/restore.sh
@@ -8,17 +8,17 @@ ENV_NAME=dev
 # aws cloudformation delete-stack --region us-east-1 --stack-name $ENV_NAME-bastionhosts
 if [ $ENV_NAME = "prod" ]; then
     echo "This script should not be run in the production environment."
-else
-
-    POSTGRESQL_PW="$(aws secretsmanager get-secret-value --secret-id crowd/${ENV_NAME}/DB/MasterUserPassword | python3 -c 'import json,sys;Secret=json.load(sys.stdin);SecretString=json.loads(Secret["SecretString"]);print(SecretString["password"])')"
-    # TODO: look up the RDS endpoint for this environment
-    POSTGRESQL_HOST=${POSTGRESQL_HOST:-localhost}
-    DUMP_FILE=/concordia.dmp
-
-    echo "${POSTGRESQL_HOST}:5432:*:concordia:${POSTGRESQL_PW}" > ~/.pgpass
-    chmod 600 ~/.pgpass
-
-    psql -U concordia -h "$POSTGRESQL_HOST" -d postgres -c "select pg_terminate_backend(pid) from pg_stat_activity where datname='concordia';"
-    psql -U concordia -h "$POSTGRESQL_HOST" -d postgres -c "drop database concordia;"
-    pg_restore --create --clean -U concordia -h "${POSTGRESQL_HOST}" -Fc --dbname=postgres --no-owner --no-acl "${DUMP_FILE}"
+    exit 1
 fi
+
+POSTGRESQL_PW="$(aws secretsmanager get-secret-value --secret-id crowd/${ENV_NAME}/DB/MasterUserPassword | python3 -c 'import json,sys;Secret=json.load(sys.stdin);SecretString=json.loads(Secret["SecretString"]);print(SecretString["password"])')"
+# TODO: look up the RDS endpoint for this environment
+POSTGRESQL_HOST=${POSTGRESQL_HOST:-localhost}
+DUMP_FILE=/concordia.dmp
+
+echo "${POSTGRESQL_HOST}:5432:*:concordia:${POSTGRESQL_PW}" > ~/.pgpass
+chmod 600 ~/.pgpass
+
+psql -U concordia -h "$POSTGRESQL_HOST" -d postgres -c "select pg_terminate_backend(pid) from pg_stat_activity where datname='concordia';"
+psql -U concordia -h "$POSTGRESQL_HOST" -d postgres -c "drop database concordia;"
+pg_restore --create --clean -U concordia -h "${POSTGRESQL_HOST}" -Fc --dbname=postgres --no-owner --no-acl "${DUMP_FILE}"
