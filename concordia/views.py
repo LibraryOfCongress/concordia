da1348a98ffe81782bbf2f0bcaef1d88f72cb556
Chris Adams <cadams@loc.gov>
2019-02-08 14:21:11 -0500

    Only display the unique tag values on the asset page
    This partially addresses
    https://github.com/LibraryOfCongress/concordia/issues/806

--- concordia/views.py
+++ concordia/views.py
@@ -680,11 +680,11 @@ class AssetDetailView(DetailView):
         )
 
         tag_groups = UserAssetTagCollection.objects.filter(asset__slug=asset.slug)
-        ctx["tags"] = tags = []
+        ctx["tags"] = tags = set()
 
         for tag_group in tag_groups:
             for tag in tag_group.tags.all():
-                tags.append(tag)
+                tags.add(tag.value)
 
         return ctx
 
