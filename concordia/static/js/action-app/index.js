cc9e2e649a7aa43633e8e3e4f9cf69382c21c121
Chris Adams <cadams@loc.gov>
2019-02-15 09:54:54 -0500

    Refactor class AssetTooltip definition
    This doesn’t depend on anything by closure so we
    can start doing it into a static location.

--- concordia/static/js/action-app/index.js
+++ concordia/static/js/action-app/index.js
@@ -14,6 +14,24 @@ let $$ = (selector, scope = document) => {
     return Array.from(scope.querySelectorAll(selector));
 };
 
+class AssetTooltip {
+    constructor() {
+        this.el = html('.asset-tooltip.text-white.p-2', [
+            html('.item-title'),
+            html('.asset-title'),
+            html('.difficulty-score-container', [
+                text('Difficulty Score: '),
+                html('span.difficulty-score')
+            ])
+        ]);
+    }
+    update(asset) {
+        $('.item-title', this.el).innerText = asset.item.title;
+        $('.asset-title', this.el).innerText = 'Image ' + asset.sequence;
+        $('.difficulty-score', this.el).innerText = asset.difficulty;
+    }
+}
+
 export class ActionApp {
     constructor(config) {
         /*
@@ -154,24 +172,6 @@ export class ActionApp {
         });
 
         /* Tooltips */
-        class AssetTooltip {
-            constructor() {
-                this.el = html('.asset-tooltip.text-white.p-2', [
-                    html('.item-title'),
-                    html('.asset-title'),
-                    html('.difficulty-score-container', [
-                        text('Difficulty Score: '),
-                        html('span.difficulty-score')
-                    ])
-                ]);
-            }
-            update(asset) {
-                $('.item-title', this.el).innerText = asset.item.title;
-                $('.asset-title', this.el).innerText =
-                    'Image ' + asset.sequence;
-                $('.difficulty-score', this.el).innerText = asset.difficulty;
-            }
-        }
         const tooltip = new AssetTooltip();
 
         this.assetList.addEventListener('mouseover', evt => {
