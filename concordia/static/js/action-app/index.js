be8b91da62a060001b8275c5f9417f37a6caf39e
Chris Adams <cadams@loc.gov>
2019-02-15 11:03:13 -0500

    Refactor item metadata display to use API w/cache
    This only retrieves related element information if
    we don’t already have a copy in the cache.

--- concordia/static/js/action-app/index.js
+++ concordia/static/js/action-app/index.js
@@ -217,6 +217,42 @@ export class ActionApp {
         });
     }
 
+    getCachedData(container, refObj, key) {
+        /*
+            Assumes a passed object with .id potentially matching a key in
+            this.items and .url being the source for the data if we don't
+            already have a copy
+
+            The key parameter will be used to extract only a single key from the
+            returned data object. This is a code-smell indicator that we may
+            want to review our API return format to have the parent/children
+            elements use the same name everywhere.
+        */
+        let id = refObj.id.toString();
+
+        if (container.has(id)) {
+            return Promise.resolve(container.get(id));
+        } else {
+            return fetchJSON(refObj.url).then(data => {
+                let obj = key ? data[key] : data;
+                container.set(id, obj);
+                return obj;
+            });
+        }
+    }
+
+    getCachedItem(refObj) {
+        return this.getCachedData(this.items, refObj, 'item');
+    }
+
+    getCachedProject(refObj) {
+        return this.getCachedData(this.projects, refObj, 'project');
+    }
+
+    getCachedCampaign(refObj) {
+        return this.getCachedData(this.campaigns, refObj, 'campaign');
+    }
+
     createAsset(assetData) {
         // n.b. although we are
         this.assets.set(assetData.id.toString(), assetData);
@@ -243,6 +279,12 @@ export class ActionApp {
     openViewer(assetElement) {
         let asset = this.assets.get(assetElement.dataset.id);
 
+        this.getCachedItem(asset.item).then(itemInfo => {
+            console.log(itemInfo);
+            $('#asset-more-info').innerHTML =
+                '<pre>' + JSON.stringify(itemInfo, null, 3) + '</pre>';
+        });
+
         this.appElement.dataset.openAssetId = asset.id;
 
         $$('.asset-title', this.assetViewer).forEach(i => {
@@ -257,14 +299,10 @@ export class ActionApp {
             i.innerText = asset.project.title;
             i.href = asset.project.url;
         });
-
         $$('.campaign-title', this.assetViewer).forEach(i => {
             i.innerText = asset.campaign.title;
             i.href = asset.campaign.url;
         });
-
-        $('#asset-more-info').innerHTML =
-            '<pre>' + JSON.stringify(asset.metadata, null, 3) + '</pre>';
 
         // This should be a component which renders based on the mode and the provided data
         if (asset.latest_transcription) {
