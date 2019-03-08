db09107aaa15efba0c2c4ccb98e42a6a99e41bdb
Rosie Storey <rstorey@loc.gov>
2019-03-08 18:09:26 -0500

    Add sample code as placeholder for asset web socket updates

--- concordia/routing.py
+++ concordia/routing.py
@@ -1,7 +1,16 @@
-from channels.routing import ProtocolTypeRouter
+# mysite/routing.py
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+from . import consumers
+
+websocket_urlpatterns = [
+    url(r"^ws/asset/(?P<asset_pk>[^/]+)/$", consumers.AssetConsumer)
+]
 
 application = ProtocolTypeRouter(
     {
-        # Empty for now (http->django views is added by default)
+        # (http->django views is added by default)
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
     }
 )
