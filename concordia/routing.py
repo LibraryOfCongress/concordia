54564f9605e5eec5ec750f0822614475a4e81fb8
Rosie Storey <rstorey@loc.gov>
2019-03-11 15:56:21 -0400

    Take out incorrect code comment

--- concordia/routing.py
+++ concordia/routing.py
@@ -1,7 +1,7 @@
-# mysite/routing.py
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
+
 from . import consumers
 
 websocket_urlpatterns = [
