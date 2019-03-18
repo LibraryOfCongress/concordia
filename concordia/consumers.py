ec2a1c464ebd43323d42f2b4b59a1653798573b3
Rosie Storey <rstorey@loc.gov>
2019-03-18 13:48:11 -0400

    Change from sync to async web socket, it works now

--- concordia/consumers.py
+++ concordia/consumers.py
@@ -1,54 +1,30 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class AssetConsumer(WebsocketConsumer):
-    def connect(self):
+class AssetConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.asset_id = self.scope["url_route"]["kwargs"]["asset_id"]
         self.asset_group_name = self.asset_id
-        print("************CONNECT*****************************************")
-        print(self.asset_group_name)
         # Join group
-        async_to_sync(
-            self.channel_layer.group_add(self.asset_group_name, self.channel_name)
-        )
+        await self.channel_layer.group_add(self.asset_group_name, self.channel_name)
+        await self.accept()
 
-        self.accept()
-
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave group
-        print("******************DISCONNECT**********************************")
-        print(self.asset_group_name)
-        async_to_sync(
-            self.channel_layer.group_discard(self.asset_group_name, self.channel_name)
-        )
+        await self.channel_layer.group_discard(self.asset_group_name, self.channel_name)
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        print("*************RECEIVE*************************")
-        print("Received message")
-        print(message)
-
         # Send message to group
-        async_to_sync(
-            self.channel_layer.group_send(
-                self.asset_group_name, {"type": "asset_update", "message": message}
-            )
+        await self.channel_layer.group_send(
+            self.asset_group_name, {"type": "asset_update", "message": message}
         )
 
     # Receive message from group
-    def asset_update(self, message):
-        print("**********************UPDATE********************")
-        print(message)
-
+    async def asset_update(self, message):
         # Send message to WebSocket
-        async_to_sync(
-            self.channel_layer.group_send(
-                self.asset_group_name,
-                {"type": "asset_update", "message": "This is an asset update message"},
-            )
-        )
+        await self.send(text_data=json.dumps({"message": message}))
