0b41e78a66921be7c1045078d38676ab7a6671f4
Rosie Storey <rstorey@loc.gov>
2019-03-19 17:25:50 -0400

    Add timestamp to AssetConsumer messages

--- concordia/consumers.py
+++ concordia/consumers.py
@@ -1,3 +1,5 @@
+import time
+
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
@@ -10,7 +12,11 @@ class AssetConsumer(AsyncJsonWebsocketConsumer):
         await self.channel_layer.group_discard("asset_updates", self.channel_name)
 
     async def asset_update(self, message):
-        await self.send_json({"message": message})
+        await self.send_json(
+            {"message": message, "message_timestamp": int(time.time())}
+        )
 
     async def asset_reservation(self, message):
-        await self.send_json({"message": message})
+        await self.send_json(
+            {"message": message, "message_timestamp": int(time.time())}
+        )
