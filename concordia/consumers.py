e946e4f3b41a4b50c3b976b890216f8ca850624e
Chris Adams <cadams@loc.gov>
2019-03-19 16:24:09 -0400

    Consumers: switch to AsyncJsonWebsocketConsumer

--- concordia/consumers.py
+++ concordia/consumers.py
@@ -1,31 +1,16 @@
-import json
-
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-class AssetConsumer(AsyncWebsocketConsumer):
+class AssetConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.channel_layer.group_add("asset_updates", self.channel_name)
         await self.accept()
 
-    async def disconnect(self, close_code):
+    async def disconnect(self, code):
         await self.channel_layer.group_discard("asset_updates", self.channel_name)
 
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        message_type = text_data_json["type"]
-
-        # Send message to group
-        await self.channel_layer.group_send(
-            "asset_updates", {"type": message_type, "message": message}
-        )
-
-    # Receive message from group
     async def asset_update(self, message):
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send_json({"message": message})
 
     async def asset_reservation(self, message):
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send_json({"message": message})
