4215388ea920fdd03c21dcd1efddd2b6dd245ce1
Rosie Storey <rstorey@loc.gov>
2019-03-20 10:29:17 -0400

    Consistently quote echo statements in docker entrypoint.sh files

--- celerybeat/entrypoint.sh
+++ celerybeat/entrypoint.sh
@@ -5,4 +5,5 @@ set -e -u # Exit immediately for unhandled errors or undefined variables
 mkdir -p /app/logs
 touch /app/logs/concordia.log
 
+echo "Running celerybeat"
 celery -A concordia beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,22 @@ set -e -u # Exit immediately for unhandled errors or undefined variables
 mkdir -p /app/logs
 touch /app/logs/concordia.log
 
-echo Running makemigrations
+echo "Running makemigrations"
 ./manage.py makemigrations --merge --noinput
 
-echo Running migrations
+echo "Running migrations"
 ./manage.py migrate
 
-echo Ensuring our base configuration is present in the database
+echo "Ensuring our base configuration is present in the database"
 ./manage.py ensure_initial_site_configuration
 
 if [ -v SENTRY_BACKEND_DSN ]; then
-    echo Testing Sentry configuration
+    echo "Testing Sentry configuration"
     ./manage.py raven test
 fi
 
-echo Running collectstatic
+echo "Running collectstatic"
 ./manage.py collectstatic --clear --noinput -v0
 
-echo Running Django dev server
-gunicorn --log-level=warn --bind 0.0.0.0:80 --workers=4 concordia.wsgi
+echo "Running Django ASGI server"
+daphne -b 0.0.0.0 -p 80 concordia.asgi:application
