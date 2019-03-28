249ae0c8aaef5f0f2be05a9d1c953bd523fb6660
Elaine Kamlley <elainekamlley@users.noreply.github.com>
2019-03-28 10:51:26 -0400

    create url path for new resources page

--- concordia/urls.py
+++ concordia/urls.py
@@ -91,6 +91,7 @@ urlpatterns = [
     path("help-center/how-to-review/", views.simple_page, name="how-to-review"),
     path("help-center/how-to-tag/", views.simple_page, name="how-to-tag"),
     path("for-educators/", views.simple_page, name="for-educators"),
+    path("resources/", views.simple_page, name="resources"),
     path(
         "latest/",
         RedirectView.as_view(pattern_name="about", permanent=True, query_string=True),
