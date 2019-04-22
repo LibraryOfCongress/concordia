0fd7c53157fdc6a820abf408be0c5499d9646cc1
Rosie Storey <rstorey@loc.gov>
2019-04-22 18:02:48 -0400

    Only run sentry test if sentry is configured

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,10 @@ echo Running migrations
 echo "Ensuring our base configuration is present in the database"
 ./manage.py ensure_initial_site_configuration
 
-echo "Sending sentry test event"
-echo "from sentry_sdk import capture_message;capture_message('This is a test event')\n" | pipenv run ./manage.py shell
+if [ -v SENTRY_BACKEND_DSN ]; then
+    echo "Testing Sentry configuration"
+    echo "from sentry_sdk import capture_message;capture_message('This is a test event')\n" | pipenv run ./manage.py shell
+fi
 
 echo Running collectstatic
 ./manage.py collectstatic --clear --noinput -v0
