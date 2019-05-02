a5114cbec22e82294b6634237fd9dee30fe59a9b
Rosie Storey <rstorey@loc.gov>
2019-05-02 14:19:44 -0400

    Add index for assets to ElasticSearch

--- concordia/documents.py
+++ concordia/documents.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django_elasticsearch_dsl import DocType, Index, fields
 
-from .models import SiteReport, Transcription, UserAssetTagCollection
+from .models import Asset, SiteReport, Transcription, UserAssetTagCollection
 
 user = Index("users")
 user.settings(number_of_shards=1, number_of_replicas=0)
@@ -15,18 +15,21 @@ transcription.settings(number_of_shards=1, number_of_replicas=0)
 site_report = Index("site_reports")
 site_report.settings(number_of_shards=1, number_of_replicas=0)
 
+asset = Index("assets")
+asset.settings(number_of_shards=1, number_of_replicas=0)
+
 
 @user.doc_type
 class UserDocument(DocType):
     class Meta:
         model = User
 
-        fields = ["last_login", "date_joined", "username"]
+        fields = ["last_login", "date_joined", "username", "is_active"]
 
 
 @site_report.doc_type
 class SiteReportDocument(DocType):
-    campaign = fields.ObjectField(properties={"slug": fields.TextField()})
+    campaign = fields.ObjectField(properties={"slug": fields.KeywordField()})
 
     class Meta:
         model = SiteReport
@@ -62,15 +65,15 @@ class TagCollectionDocument(DocType):
         properties={
             "title": fields.TextField(),
             "slug": fields.TextField(),
-            "transcription_status": fields.TextField(),
+            "transcription_status": fields.KeywordField(),
             "item": fields.ObjectField(
                 properties={
                     "item_id": fields.TextField(),
                     "project": fields.ObjectField(
                         properties={
-                            "slug": fields.TextField(),
+                            "slug": fields.KeywordField(),
                             "campaign": fields.ObjectField(
-                                properties={"slug": fields.TextField()}
+                                properties={"slug": fields.KeywordField()}
                             ),
                         }
                     ),
@@ -101,15 +104,15 @@ class TranscriptionDocument(DocType):
         properties={
             "title": fields.TextField(),
             "slug": fields.TextField(),
-            "transcription_status": fields.TextField(),
+            "transcription_status": fields.KeywordField(),
             "item": fields.ObjectField(
                 properties={
                     "item_id": fields.TextField(),
                     "project": fields.ObjectField(
                         properties={
-                            "slug": fields.TextField(),
+                            "slug": fields.KeywordField(),
                             "campaign": fields.ObjectField(
-                                properties={"slug": fields.TextField()}
+                                properties={"slug": fields.KeywordField()}
                             ),
                         }
                     ),
@@ -117,8 +120,8 @@ class TranscriptionDocument(DocType):
             ),
         }
     )
-    user = fields.ObjectField(properties={"username": fields.TextField()})
-    reviewed_by = fields.ObjectField(properties={"username": fields.TextField()})
+    user = fields.ObjectField(properties={"username": fields.KeywordField()})
+    reviewed_by = fields.ObjectField(properties={"username": fields.KeywordField()})
     supersedes = fields.ObjectField(properties={"id": fields.IntegerField()})
 
     class Meta:
@@ -142,4 +145,35 @@ class TranscriptionDocument(DocType):
             .prefetch_related(
                 "asset__item", "asset__item__project", "asset__item__project__campaign"
             )
+        )
+
+
+@asset.doc_type
+class AssetDocument(DocType):
+    item = fields.ObjectField(
+        properties={
+            "item_id": fields.KeywordField(),
+            "project": fields.ObjectField(
+                properties={
+                    "slug": fields.KeywordField(),
+                    "campaign": fields.ObjectField(
+                        properties={"slug": fields.KeywordField()}
+                    ),
+                }
+            ),
+        }
+    )
+
+    transcription_status = fields.KeywordField()
+
+    class Meta:
+        model = Asset
+        fields = ["published", "difficulty", "slug", "sequence", "year"]
+
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .order_by("pk")
+            .prefetch_related("item", "item__project", "item__project__campaign")
         )
