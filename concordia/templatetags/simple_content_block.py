73e6380ddf37efc932df203ce7d423f10adb3e6a
Rosie Storey <rstorey@loc.gov>
2019-06-19 14:45:46 -0400

    Catch DoesNotExist exception if simple content block isn't defined

--- concordia/templatetags/simple_content_block.py
+++ concordia/templatetags/simple_content_block.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.exceptions import ObjectDoesNotExist
 
 from ..models import SimpleContentBlock
 
@@ -7,5 +8,10 @@ register = template.Library()
 
 @register.simple_tag()
 def simple_content_block(block_label):
-    content_block = SimpleContentBlock.objects.get(label=block_label)
-    return content_block.body
+    content_block_body = ""
+    try:
+        content_block = SimpleContentBlock.objects.get(label=block_label)
+        content_block_body = content_block.body
+    except ObjectDoesNotExist:
+        pass
+    return content_block_body
