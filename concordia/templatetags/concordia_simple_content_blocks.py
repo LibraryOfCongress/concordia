c38c10fa7d7ab6a3b295eba3194cd0f9ee41d34f
Chris Adams <cadams@loc.gov>
2019-06-20 17:02:35 -0400

    Allow Django template markup in SimpleContentBlocks
    This allows migrating existing content wholesale but may not be worth the extra maintenance overhead.

--- concordia/templatetags/concordia_simple_content_blocks.py
+++ concordia/templatetags/concordia_simple_content_blocks.py
@@ -1,18 +1,21 @@
-from django import template
 from django.core.exceptions import ObjectDoesNotExist
+from django.template import Library, Template
 from django.utils.safestring import mark_safe
 
 from ..models import SimpleContentBlock
 
-register = template.Library()
+register = Library()
 
 
-@register.simple_tag()
-def simple_content_block(block_label):
+@register.simple_tag(takes_context=True)
+def simple_content_block(context, block_label):
     try:
         content_block = SimpleContentBlock.objects.get(label=block_label)
-        # SimpleContentBlocks always contain HTML and they are entered by admins
-        # and processed through Bleach:
-        return mark_safe(content_block.body)  # nosec
     except ObjectDoesNotExist:
         return ""
+
+    # SimpleContentBlocks always contain HTML and they are entered by admins and
+    # processed through Bleach so we'll mark the output as safe to avoid
+    # double-escaping:
+    template = Template(content_block.body)
+    return mark_safe(template.render(context))  # nosec
