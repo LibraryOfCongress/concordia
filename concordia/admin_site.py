50642caee66a5537f5ff64c20166031e483ec233
Rosie Storey <rstorey@loc.gov>
2019-11-27 15:46:51 -0500

    Move warning banner from admin site dashbaord to admin login template

--- concordia/admin_site.py
+++ concordia/admin_site.py
@@ -5,6 +5,7 @@ from django.urls import path
 class ConcordiaAdminSite(admin.AdminSite):
     site_header = "Concordia Admin"
     site_title = "Concordia"
+    login_template = "admin/auth/admin_login.html"
 
     def get_urls(self):
         from concordia.admin.views import admin_bulk_import_view, admin_site_report_view
