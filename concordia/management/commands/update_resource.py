7e2179a757c9c6f816c77caecb4aad00446921b0
Rosie Storey <rstorey@loc.gov>
2020-10-06 18:17:00 -0400

    use list for string split results

--- concordia/management/commands/update_resource.py
+++ concordia/management/commands/update_resource.py
@@ -7,19 +7,12 @@ class Command(BaseCommand):
     def handle(self, *, verbosity, **kwargs):
         assets = Asset.objects.filter(item__item_id="2010414646")
         for asset in assets:
-            (
-                first,
-                second,
-                third,
-                fourth,
-                fifth,
-                sixth,
-            ) = asset.download_url.split(":")
+            url_pieces = asset.download_url.split(":")
             convert_resource = (
                 asset.resource_url[0:37]
                 + asset.download_url[66:82].replace(":", "")
                 + "?sp="
-                + sixth[0:3]
+                + url_pieces[5][0:3]
             )
             asset.resource_url = convert_resource
             print(asset.resource_url)
