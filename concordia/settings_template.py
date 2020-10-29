c1b44401e0a2b0380333fc1ceb220a7bd2effc32
rabiloc <rabi@loc.gov>
2020-10-29 12:32:10 -0400

    remove default place holder values

--- concordia/settings_template.py
+++ concordia/settings_template.py
@@ -334,7 +334,7 @@ ROBOTS_USE_SITEMAP = False
 ROBOTS_USE_HOST = False
 
 # django-bootstrap4 customization:
-BOOTSTRAP4 = {"required_css_class": "form-group-required"}
+BOOTSTRAP4 = {"required_css_class": "form-group-required", "set_placeholder": False}
 
 # Transcription-related settings
 
