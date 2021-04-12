0cf45184ccf3b2ac85d9cbd42ad9b69548eabdb6
Jennifer Kuenning <jkue@loc.gov>
2021-04-12 10:19:04 -0400

    remove --clean and echo final return code

--- db_scripts/restore/restore.sh
+++ db_scripts/restore/restore.sh
@@ -27,4 +27,5 @@ aws s3 sync s3://crowd-content s3://crowd-${ENV_NAME}-content
 
 psql -U concordia -h "$POSTGRESQL_HOST" -d postgres -c "select pg_terminate_backend(pid) from pg_stat_activity where datname='concordia';"
 psql -U concordia -h "$POSTGRESQL_HOST" -d postgres -c "drop database concordia;"
-pg_restore --create --clean -U concordia -h "${POSTGRESQL_HOST}" -Fc --dbname=postgres --no-owner --no-acl "${DUMP_FILE}"
+pg_restore --create -U concordia -h "${POSTGRESQL_HOST}" -Fc --dbname=postgres --no-owner --no-acl "${DUMP_FILE}"
+echo $?
