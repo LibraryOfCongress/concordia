f155dee5f67aa307a14534be41562cbd50e42d2e
Jennifer Kuenning <72825410+jkueloc@users.noreply.github.com>
2022-04-19 08:25:07 -0400

    add sorting by sequence to Resource (#1680)

--- concordia/views.py
+++ concordia/views.py
@@ -296,10 +296,9 @@ def ratelimit_view(request, exception=None):
 @login_required
 @never_cache
 def AccountLetterView(request):
-    """
-    Generates a transcriptions and reviews contribution pdf letter for the
-    user and downloads it
-    """
+    # Generates a transcriptions and reviews contribution pdf letter
+    # for the user and downloads it
+
     date_today = datetime.datetime.now()
     username = request.user.email
     join_date = request.user.date_joined
@@ -921,9 +920,9 @@ class CampaignDetailView(APIDetailView):
         ctx = super().serialize_context(context)
         ctx["object"]["related_links"] = [
             {"title": title, "url": url}
-            for title, url in self.object.resource_set.order_by(
-                "-sequence"
-            ).values_list("title", "resource_url")
+            for title, url in self.object.resource_set.values_list(
+                "title", "resource_url"
+            )
         ]
         return ctx
 
