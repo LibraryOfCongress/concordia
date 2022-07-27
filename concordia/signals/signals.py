bc4a6444c62b2efbaf619413b744435686f4ade1
rasarkar <105652044+rasarkar@users.noreply.github.com>
2022-07-27 14:57:41 -0400

    Merge upgrade branch into main (#1715)
    * add django-upgrade to pre-commit
    
    * django-upgrade target 3.2 updates post format updates
    
    * update version: Django and django-elasticsearch-dsl
    
    * v3.2 default auto-created primary keys
    
    * v3.0 staticfiles require libraries registration
    
    * v3.0 staticfiles require libraries registration
    
    * django 3.1 replace deprecated password timeout setting
    
    * to avoid BigAutoField M2M migration issue #31990 set to AutoField
    
    * remove v3.2 depreciated python-memcache
    
    * install pymemcache remove deprecated python-memcached
    
    * release pin on psycopg2 version
    
    * use SemVer on Django in Pipfile
    
    * django 3.1 deprecation of PostgreSQL JSONField
    
    * Django 3.1 deprecation of Signal providing_args
    
    * for now pin asgiref to 3.5.1 breaking change in 3.5.2 - Single thread executor already being used, would deadlock
    
    * removed pin of asgiref due to PR #1690 Bumping pillow
    
    * Change python 3.8 to 3.10
    
    * celery regression 4878 closed
    
    * add annotation to queryset, before attempting to use it (#1699)
    
    * add annotation to queryset, before attempting to use it
    
    * added a unittest for AccountProfileView.get_queryset
    
    * pin elasticsearch version to avoid UnsupportedProductError
    https://github.com/elastic/elasticsearch-py/issues/1667
    
    * HttpRequest.is_ajax is deprecated (#1701)
    
    * HttpRequest.is_ajax is deprecated
    
    * added unit test for ratelimit_view function
    
    * just want to confirm that the correct response is being returned
    
    * update aws-es-proxy to lastest
    
    * update aws-es-proxy version
    
    Co-authored-by: Jennifer Kuenning <jkue@loc.gov>

--- concordia/signals/signals.py
+++ concordia/signals/signals.py
@@ -1,9 +1,8 @@
 import django.dispatch
 
-reservation_obtained = django.dispatch.Signal(
-    providing_args=["asset_pk", "reservation_token"]
-)
+# signals notify channels of asset reservation - kwargs from handlers
+# ["asset_pk", "reservation_token"]
 
-reservation_released = django.dispatch.Signal(
-    providing_args=["asset_pk", "reservation_token"]
-)
+reservation_obtained = django.dispatch.Signal()
+
+reservation_released = django.dispatch.Signal()
