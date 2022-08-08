585966fbb71dcbd0263c863045ea73b448dc8bdc
Jennifer Kuenning <jkue@loc.gov>
2022-08-08 12:24:41 -0400

    CONCD-137 reduce kibana-proxy image size

--- kibana-proxy/build_and_push.sh
+++ kibana-proxy/build_and_push.sh
@@ -5,6 +5,6 @@ set -eu -o pipefail
 AWS_ACCOUNT_ID="$(aws sts get-caller-identity  --output=text --query "Account")"
 eval "$(aws ecr get-login --no-include-email --region us-east-1)"
 
-docker build -t concordia/kibana-proxy .
+docker build --no-cache --pull -t concordia/kibana-proxy .
 docker tag concordia/kibana-proxy:latest "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/kibana-proxy:latest"
 docker push "${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/concordia/kibana-proxy:latest"
