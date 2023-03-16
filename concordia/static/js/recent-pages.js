0b8f51338e1a63b7e5f27ba5159e505937968b2d
Rajit Sarkar <rsar@loc.gov>
2023-03-16 15:31:23 -0400

    CONCD-190, 194 reload just the table, not the entire page

--- concordia/static/js/recent-pages.js
+++ concordia/static/js/recent-pages.js
@@ -1,15 +1,15 @@
 /* global jQuery */
 
-function getPages() {
+function getPages(queryString = window.location.search) {
     jQuery.ajax({
         type: 'GET',
-        url: '/account/get_pages/' + window.location.search,
+        url: '/account/get_pages/' + queryString,
         dataType: 'json',
         success: function (data) {
             var recentPages = document.createElement('div');
             recentPages.setAttribute('class', 'col-md');
             recentPages.innerHTML = data.content;
-            document.getElementById('recent-pages').append(recentPages);
+            jQuery('#recent-pages').html(recentPages);
         },
     });
 }
