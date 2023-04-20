8d10386da6e0879e77e65b5f0de75929ee8b8704
Josh Stegmaier <104993387+joshuastegmaier@users.noreply.github.com>
2023-04-20 13:00:04 -0400

    Added task to populate ResourceFiles from existing files on S3 (#1960)

--- concordia/tasks.py
+++ concordia/tasks.py
@@ -1,6 +1,8 @@
 import datetime
+import os.path
 from logging import getLogger
 
+import boto3
 from celery import chord
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -17,6 +19,7 @@ from concordia.models import (
     CampaignRetirementProgress,
     Item,
     Project,
+    ResourceFile,
     SiteReport,
     Tag,
     Topic,
@@ -770,3 +773,22 @@ def delete_asset(asset_id):
     logger.debug("Asset %s (%s) deleted" % (asset, asset_id))
 
     return asset_id
+
+
+@celery_app.task(ignore_result=True)
+def populate_resource_files():
+    client = boto3.client("s3")
+    response = client.list_objects_v2(
+        Bucket=settings.S3_BUCKET_NAME, Prefix="cm-uploads/resources/"
+    )
+    files = response["Contents"]
+    for file in files:
+        path = file["Key"]
+        if path == "cm-uploads/resources/":
+            continue
+        try:
+            ResourceFile.objects.get(resource=path)
+        except ResourceFile.DoesNotExist:
+            filename, extension = os.path.splitext(os.path.basename(path))
+            name = "%s-%s" % (filename, extension[1:])
+            ResourceFile.objects.create(resource=path, name=name)
