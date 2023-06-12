34a80df560c6d24cc8bc8112dff2602b7c664f86
Jennifer Kuenning <72825410+jkueloc@users.noreply.github.com>
2023-06-12 14:07:39 -0400

    CONCD-422 update push scripts (#1999)

--- db_scripts/restore/push-container.sh
+++ db_scripts/restore/push-container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export AWS_PROFILE=AWSCrowdlocgov
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 619333082511.dkr.ecr.us-east-1.amazonaws.com
 docker build --no-cache --pull -t crowd-db-restore .
 docker tag crowd-db-restore:latest 619333082511.dkr.ecr.us-east-1.amazonaws.com/crowd-db-restore:latest
