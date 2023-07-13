2702a9bd812aba76d7afb398fab2de0501378c6b
rasarkar <105652044+rasarkar@users.noreply.github.com>
2023-07-13 11:02:05 -0400

    CONCD-262 truncate on the left (rather than the right) (#2031)

--- concordia/templatetags/concordia_text_tags.py
+++ concordia/templatetags/concordia_text_tags.py
@@ -14,3 +14,19 @@ def normalize_whitespace(text):
     a single space
     """
     return WHITESPACE_NORMALIZER.sub(" ", text)
+
+
+@register.filter
+def truncate_left(value):
+    """
+    Similar to the built-in truncatewords filter, but instead
+    truncate before `arg` number of words (rather than after).
+    """
+    truncate = len(value) > 26
+    while len(value) > 26:
+        words = value.split()
+        words = words[1:]
+        value = " ".join(words)
+    if truncate:
+        value = "...%s" % value
+    return value
