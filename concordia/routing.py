a27d699740fe0072ae1a9da717d42a781cb084b3
joshuastegmaier <jstegmaier@loc.gov>
2023-11-17 13:26:43 -0500

    Added Django handler to daphne config

--- concordia/routing.py
+++ concordia/routing.py
@@ -1,14 +1,20 @@
+import os
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 from django.urls import path
 
 from . import consumers
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "concordia.settings")
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter(
     {
-        # (http->django views is added by default)
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter([path("ws/asset/asset_updates/", consumers.AssetConsumer)])
-        )
+        ),
     }
 )
