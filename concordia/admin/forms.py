2a3de14058106089fd229027b7b6588aca3d33a0
Rajit Sarkar <rsar@loc.gov>
2023-12-28 14:34:13 -0500

    CONCD-646 Replace bleach HTML form sanitizer with nh3

--- concordia/admin/forms.py
+++ concordia/admin/forms.py
@@ -1,10 +1,26 @@
-import bleach
+import nh3
 from django import forms
 from tinymce.widgets import TinyMCE
 
 from ..models import Campaign, Project
 
-FRAGMENT_ALLOWED_TAGS = {"br", "kbd", "span"} | bleach.sanitizer.ALLOWED_TAGS
+FRAGMENT_ALLOWED_TAGS = {
+    "a",
+    "abbr",
+    "acronym",
+    "b",
+    "blockquote",
+    "br",
+    "code",
+    "em",
+    "i",
+    "kbd",
+    "li",
+    "ol",
+    "span",
+    "strong",
+    "ul",
+}
 
 BLOCK_ALLOWED_TAGS = FRAGMENT_ALLOWED_TAGS | {
     "div",
@@ -20,8 +36,9 @@ BLOCK_ALLOWED_TAGS = FRAGMENT_ALLOWED_TAGS | {
 }
 
 ALLOWED_ATTRIBUTES = {
-    **bleach.sanitizer.ALLOWED_ATTRIBUTES,
     "a": ["class", "id", "href", "title"],
+    "abbr": ["title"],
+    "acronym": ["title"],
     "div": ["class", "id"],
     "span": ["class", "id"],
     "p": ["class", "id"],
@@ -54,14 +71,14 @@ class AdminRedownloadImagesForm(forms.Form):
 
 class BleachedDescriptionAdminForm(forms.ModelForm):
     def clean_description(self):
-        return bleach.clean(
+        return nh3.clean(
             self.cleaned_data["description"],
             tags=BLOCK_ALLOWED_TAGS,
             attributes=ALLOWED_ATTRIBUTES,
         )
 
     def clean_short_description(self):
-        return bleach.clean(
+        return nh3.clean(
             self.cleaned_data["short_description"],
             tags=FRAGMENT_ALLOWED_TAGS,
             attributes=ALLOWED_ATTRIBUTES,
@@ -89,7 +106,7 @@ class ProjectAdminForm(BleachedDescriptionAdminForm):
 
 class SimpleContentBlockAdminForm(forms.ModelForm):
     def clean_body(self):
-        return bleach.clean(
+        return nh3.clean(
             self.cleaned_data["body"],
             tags=BLOCK_ALLOWED_TAGS,
             attributes=ALLOWED_ATTRIBUTES,
