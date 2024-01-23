6f76501e33ebe02790a174dae28ac8c0407de9c3
rasarkar <105652044+rasarkar@users.noreply.github.com>
2024-01-23 12:48:53 -0500

    CONCD-651 Share data between simple pages and guides (#2242)
    * CONCD-651 Share data between simple pages and guides
    
    * CONCD-650 slide indices were off by 1
    
    * CONCD-651 fixing an issue where view tests were failing

--- concordia/views.py
+++ concordia/views.py
@@ -1189,6 +1189,17 @@ class AssetDetailView(APIDetailView):
 
         return asset_qs
 
+    def get_guides(self):
+        guides = list(Guide.objects.order_by("order").values("title", "body"))
+        titles = [guide["title"].lower() for guide in guides]
+        for title in ("how to transcribe", "how to review", "how to tag"):
+            if title not in titles:
+                pages = SimplePage.objects.filter(title__iexact=title)
+                if pages.count() > 0:
+                    page = pages.first()
+                    guides.append({"title": page.title, "body": page.body})
+        return guides
+
     def get_context_data(self, **kwargs):
         """
         Handle the GET request
@@ -1300,7 +1311,7 @@ class AssetDetailView(APIDetailView):
             ordered_cards = unordered_cards.order_by("order")
             ctx["cards"] = [tutorial_card.card for tutorial_card in ordered_cards]
 
-        ctx["guides"] = Guide.objects.order_by("order")
+        ctx["guides"] = self.get_guides()
 
         return ctx
 
