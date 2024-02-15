418925cc1db6dcab775e74d3f80158abbd9c3aff
joshuastegmaier <jstegmaier@loc.gov>
2024-02-15 13:44:38 -0500

    Added tests for publish and unpublish admin actions. Made the published argument be passed to other objects created through the tests.utils create_* functions

--- concordia/tests/test_admin_actions.py
+++ concordia/tests/test_admin_actions.py
@@ -4,11 +4,19 @@ from django.test import TestCase
 
 from concordia.admin.actions import (
     anonymize_action,
+    publish_action,
     publish_item_action,
+    unpublish_action,
     unpublish_item_action,
 )
-from concordia.models import Asset, Item
-from concordia.tests.utils import CreateTestUsers, create_asset, create_item
+from concordia.models import Asset, Campaign, Item, Project
+from concordia.tests.utils import (
+    CreateTestUsers,
+    create_asset,
+    create_campaign,
+    create_item,
+    create_project,
+)
 
 
 class MockModelAdmin:
@@ -111,3 +119,124 @@ class ItemAdminActionTest(TestCase):
         self.assertFalse(item3.published)
         self.assertFalse(asset3.published)
         self.assertFalse(asset4.published)
+
+
+class AdminActionTest(TestCase):
+    def _setUp(self, published=True):
+        self.asset1 = create_asset(published=published)
+        self.item1 = self.asset1.item
+        self.project1 = self.item1.project
+        self.campaign1 = self.project1.campaign
+
+        self.campaign2 = create_campaign(
+            slug="test-campaign-slug-2", published=published
+        )
+        self.project2 = create_project(
+            campaign=self.campaign2, slug="test-project-slug-2", published=published
+        )
+        self.item2 = create_item(
+            project=self.project2, item_id="2", published=published
+        )
+        self.asset2 = create_asset(
+            item=self.item2, slug="test-asset-slug-2", published=published
+        )
+
+        self.campaign3 = create_campaign(
+            slug="test-campaign-slug-3", published=published
+        )
+        self.project3 = create_project(
+            campaign=self.campaign3, slug="test-project-slug-3", published=published
+        )
+        self.item3 = create_item(
+            project=self.project3, item_id="3", published=published
+        )
+        self.asset3 = create_asset(
+            item=self.item3, slug="test-asset-slug-3", published=published
+        )
+        self.asset4 = create_asset(
+            item=self.item3, slug="test-asset-slug-4", published=published
+        )
+
+    def test_publish_action(self):
+        self._setUp(False)
+        queryset = Campaign.objects.filter(
+            pk__in=[self.campaign1.pk, self.campaign3.pk]
+        )
+        publish_action(modeladmin, request, queryset)
+        campaign1 = Campaign.objects.get(pk=self.campaign1.pk)
+        campaign2 = Campaign.objects.get(pk=self.campaign2.pk)
+        campaign3 = Campaign.objects.get(pk=self.campaign3.pk)
+        project1 = Project.objects.get(pk=self.project1.pk)
+
+        self.assertTrue(campaign1.published)
+        self.assertFalse(campaign2.published)
+        self.assertTrue(campaign3.published)
+        self.assertFalse(project1.published)
+
+        queryset = Project.objects.filter(pk__in=[self.project2.pk])
+        publish_action(modeladmin, request, queryset)
+        project1 = Project.objects.get(pk=self.project1.pk)
+        project2 = Project.objects.get(pk=self.project2.pk)
+        project3 = Project.objects.get(pk=self.project3.pk)
+        item2 = Item.objects.get(pk=self.item2.pk)
+
+        self.assertFalse(project1.published)
+        self.assertTrue(project2.published)
+        self.assertFalse(project3.published)
+        self.assertFalse(item2.published)
+
+        queryset = Asset.objects.filter(
+            pk__in=[self.asset1.pk, self.asset2.pk, self.asset3.pk]
+        )
+        publish_action(modeladmin, request, queryset)
+        asset1 = Asset.objects.get(pk=self.asset1.pk)
+        asset2 = Asset.objects.get(pk=self.asset2.pk)
+        asset3 = Asset.objects.get(pk=self.asset3.pk)
+        asset4 = Asset.objects.get(pk=self.asset4.pk)
+
+        self.assertTrue(asset1.published)
+        self.assertTrue(asset2.published)
+        self.assertTrue(asset3.published)
+        self.assertFalse(asset4.published)
+
+    def test_unpublish_action(self):
+        self._setUp(True)
+        queryset = Campaign.objects.filter(
+            pk__in=[self.campaign1.pk, self.campaign3.pk]
+        )
+        unpublish_action(modeladmin, request, queryset)
+        campaign1 = Campaign.objects.get(pk=self.campaign1.pk)
+        campaign2 = Campaign.objects.get(pk=self.campaign2.pk)
+        campaign3 = Campaign.objects.get(pk=self.campaign3.pk)
+        project1 = Project.objects.get(pk=self.project1.pk)
+
+        self.assertFalse(campaign1.published)
+        self.assertTrue(campaign2.published)
+        self.assertFalse(campaign3.published)
+        self.assertTrue(project1.published)
+
+        queryset = Project.objects.filter(pk__in=[self.project2.pk])
+        unpublish_action(modeladmin, request, queryset)
+        project1 = Project.objects.get(pk=self.project1.pk)
+        project2 = Project.objects.get(pk=self.project2.pk)
+        project3 = Project.objects.get(pk=self.project3.pk)
+        item2 = Item.objects.get(pk=self.item2.pk)
+
+        self.assertTrue(project1.published)
+        self.assertFalse(project2.published)
+        self.assertTrue(project3.published)
+        self.assertTrue(item2.published)
+
+        queryset = Asset.objects.filter(
+            pk__in=[self.asset1.pk, self.asset2.pk, self.asset3.pk]
+        )
+        unpublish_action(modeladmin, request, queryset)
+        asset1 = Asset.objects.get(pk=self.asset1.pk)
+        asset2 = Asset.objects.get(pk=self.asset2.pk)
+        asset3 = Asset.objects.get(pk=self.asset3.pk)
+        asset4 = Asset.objects.get(pk=self.asset4.pk)
+
+        self.assertFalse(asset1.published)
+        self.assertFalse(asset2.published)
+        self.assertFalse(asset3.published)
+        self.assertTrue(asset4.published)
