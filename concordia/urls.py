1f8e4cb2352f6b03ef5fdda422f92352fb13546c
Rajit Sarkar <rsar@loc.gov>
2024-02-29 14:00:58 -0500

    CONCD-697 none of the urls or views are used

--- concordia/urls.py
+++ concordia/urls.py
@@ -194,10 +194,6 @@ urlpatterns = [
         name="generate-ocr-transcription",
     ),
     path("assets/<int:asset_pk>/tags/submit/", views.submit_tags, name="submit-tags"),
-    path(
-        "transcribe/", views.TranscribeListView.as_view(), name="transcribe-asset-list"
-    ),
-    path("review/", views.ReviewListView.as_view(), name="review-asset-list"),
     path("account/ajax-status/", views.ajax_session_status, name="ajax-session-status"),
     path("account/ajax-messages/", views.ajax_messages, name="ajax-messages"),
     path(
