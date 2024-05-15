061d0611e39e3dfe334f67fce4626639d3f302db
joshuastegmaier <jstegmaier@loc.gov>
2024-05-15 09:45:35 -0400

    Added additional admin.views tests

--- concordia/tests/test_admin_views.py
+++ concordia/tests/test_admin_views.py
@@ -5,9 +5,16 @@ from unittest import mock
 
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
+from django.utils import timezone
 
 from concordia.admin.views import SerializedObjectView
-from concordia.tests.utils import CreateTestUsers, create_asset, create_card
+from concordia.tests.utils import (
+    CreateTestUsers,
+    StreamingTestMixin,
+    create_asset,
+    create_card,
+    create_site_report,
+)
 
 
 @mock.patch("importer.utils.excel.load_workbook", autospec=True)
@@ -182,7 +189,50 @@ class TestRedownloadImagesView(CreateTestUsers, TestCase):
             self.assertEqual(response.context["assets_to_download"], [])
 
 
-class TestFunctionBasedViews(CreateTestUsers, TestCase):
+class TestProjectLevelExportView(CreateTestUsers, TestCase):
+    def setUp(self):
+        self.login_user(is_staff=True, is_superuser=True)
+        self.url = reverse("admin:project-level-export")
+        self.asset = create_asset(download_url="http://example.com/1234.jpg")
+        self.asset2 = create_asset(
+            slug="asset-2",
+            item=self.asset.item,
+            download_url="http://example.com/5678.jpg",
+        )
+        self.asset3 = create_asset(
+            slug="asset-3",
+            item=self.asset.item,
+            download_url="http://example.com/9012.jpg",
+        )
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response, f"<td>{self.asset.item.project.campaign.title}</td>", html=True
+        )
+
+    def test_get_campaign(self):
+        response = self.client.get(
+            self.url, {"id": self.asset.item.project.campaign.id}
+        )
+        self.assertContains(
+            response, f"<td>{self.asset.item.project.title}</td>", html=True
+        )
+
+    def test_post(self):
+        with mock.patch("exporter.views.boto3.resource", autospec=True) as bucket_mock:
+            # The parameter is 'project_name', but it actually expects the project id.
+            response = self.client.post(
+                f"{self.url}?slug={self.asset.item.project.campaign.slug}",
+                {"project_name": f"{self.asset.item.project.id}"},
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response["Content-Type"], "application/zip")
+            self.assertFalse(bucket_mock.called)
+
+
+class TestFunctionBasedViews(CreateTestUsers, TestCase, StreamingTestMixin):
     def test_admin_bulk_import_review(self):
         self.login_user(is_staff=True, is_superuser=True)
         self.assertTrue(self.user.is_active)
@@ -195,6 +245,51 @@ class TestFunctionBasedViews(CreateTestUsers, TestCase):
         data = {}
         response = self.client.post(path, data=data)
         self.assertEqual(response.status_code, 200)
+
+    def test_admin_site_report_view(self):
+        self.login_user(is_staff=True, is_superuser=True)
+        mocked_datetime = timezone.now()
+        mocked_datetime_formatted = mocked_datetime.isoformat()
+        with mock.patch("django.utils.timezone.now") as now_mocked:
+            now_mocked.return_value = mocked_datetime
+            create_site_report()
+
+        response = self.client.get(reverse("admin:site-report"))
+        self.assertEqual(response.status_code, 200)
+        content = self.get_streaming_content(response).split(b"\r\n")
+        self.assertEqual(len(content), 3)  # Includes empty line at the end of the file
+        test_data = [
+            b"Date,report name,Campaign,topic__title,assets total,assets published,"
+            b"assets not started,assets in progress,assets waiting review,"
+            b"assets completed,assets unpublished,items published,items unpublished,"
+            b"projects published,projects unpublished,anonymous transcriptions,"
+            b"transcriptions saved,daily review actions,distinct tags,tag uses,"
+            b"campaigns published,campaigns unpublished,users registered,"
+            b"users activated,registered contributors,daily active users",
+            b"%s,,,,,,,,,,,,,,,,,,,,,,,,," % str.encode(mocked_datetime_formatted),
+            b"",
+        ]
+        self.assertEqual(content, test_data)
+
+    def test_admin_retired_site_report_view(self):
+        self.login_user(is_staff=True, is_superuser=True)
+
+        response = self.client.get(reverse("admin:retired-site-report"))
+        self.assertEqual(response.status_code, 200)
+        content = self.get_streaming_content(response).split(b"\r\n")
+        self.assertEqual(len(content), 3)  # Includes empty line at the end of the file
+        test_data = [
+            b"Date,report name,Campaign,topic__title,assets total,assets published,"
+            b"assets not started,assets in progress,assets waiting review,"
+            b"bassets completed,assets unpublished,items published,items unpublished,"
+            b"projects published,projects unpublished,anonymous transcriptions,"
+            b"transcriptions saved,daily review actions,distinct tags,tag uses,"
+            b"campaigns published,campaigns unpublished,users registered,"
+            b"users activated,registered contributors,daily active users",
+            b",RETIRED TOTAL,,,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
+            b"",
+        ]
+        self.assertEqual(content, test_data)
 
 
 class TestSerializedObjectView(TestCase):
