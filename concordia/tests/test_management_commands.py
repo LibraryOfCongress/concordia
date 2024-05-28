671c6a354a9e57fef6a01ff863dd22f5bc82443e
Rajit Sarkar <rsar@loc.gov>
2024-05-28 09:31:26 -0400

    CONCD-730 unit test for import_site_reports (WiP)

--- concordia/tests/test_management_commands.py
+++ concordia/tests/test_management_commands.py
@@ -3,9 +3,25 @@ from io import StringIO
 from django.core.management import call_command
 from django.test import TestCase
 
+from concordia.tests.utils import create_asset
+
+
+class ImportSiteReportsTests(TestCase):
+    def test_command_output(self, *args, **kwargs):
+        out = StringIO()
+        call_command(
+            "import_site_reports",
+            csv_file="concordia/tests/data/site_reports.csv",
+            stdout=out,
+        )
+
 
 class PrintFrontendTestUrlsTests(TestCase):
     def test_command_output(self, *args, **kwargs):
         out = StringIO()
+        call_command("print_frontend_test_urls", stdout=out)
+        self.assertIn("", out.getvalue())
+
+        create_asset()
         call_command("print_frontend_test_urls", stdout=out)
         self.assertIn("", out.getvalue())
