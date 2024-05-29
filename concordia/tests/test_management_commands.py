8d3a33069ba557d1d31638ee3dea9d18f553103d
Rajit Sarkar <rsar@loc.gov>
2024-05-29 10:33:41 -0400

    CONCD-730 added test csv file

--- concordia/tests/test_management_commands.py
+++ concordia/tests/test_management_commands.py
@@ -3,12 +3,13 @@ from io import StringIO
 from django.core.management import call_command
 from django.test import TestCase
 
-from concordia.tests.utils import create_asset
+from concordia.tests.utils import create_asset, create_campaign
 
 
 class ImportSiteReportsTests(TestCase):
     def test_command_output(self, *args, **kwargs):
         out = StringIO()
+        create_campaign(id=1)
         call_command(
             "import_site_reports",
             csv_file="concordia/tests/data/site_reports.csv",
