2e0373cd754ef23f89f61965473a4a13d667362c
Rajit Sarkar <rsar@loc.gov>
2024-06-07 10:43:03 -0400

    CONCD-824 fleshed out unit test

--- concordia/tests/test_models.py
+++ concordia/tests/test_models.py
@@ -98,6 +98,19 @@ class TranscriptionManagerTestCase(CreateTestUsers, TestCase):
         transcriptions = Transcription.objects.reviewing_too_quickly()
         self.assertEqual(len(transcriptions), 0)
 
+        self.transcription1.accepted = timezone.now()
+        self.transcription1.reviewed_by = self.create_user(username="tester2")
+        self.transcription1.save()
+        transcription3 = create_transcription(
+            asset=self.transcription1.asset,
+            user=self.transcription1.user,
+            reviewed_by=self.transcription1.reviewed_by,
+            accepted=self.transcription1.accepted,
+        )
+        transcriptions = Transcription.objects.reviewing_too_quickly()
+        self.assertEqual(len(transcriptions), 1)
+        self.assertIn(transcription3.id, [n[2] for n in transcriptions])
+
     def test_transcribing_too_quickly(self):
         transcriptions = Transcription.objects.transcribing_too_quickly()
         self.assertEqual(len(transcriptions), 0)
