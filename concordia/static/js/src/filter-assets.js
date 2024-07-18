02942be14ccf7da802ef16ef17748761c7e0722a
Rajit Sarkar <rsar@loc.gov>
2024-07-18 09:17:48 -0400

    CONCD-868 explicitly toggling radio button

---
+++ concordia/static/js/src/filter-assets.js
@@ -0,0 +1,11 @@
+/* exported filterAssets */
+function filterAssets(filterAssets, url) {
+    var button;
+    if (filterAssets) {
+        button = document.getElementById('show-all');
+    } else {
+        button = document.getElementById('filter-assets');
+    }
+    button.checked = false;
+    window.location = url;
+}
