45e146aced1e67680755412b8d83300a680caeb6
rasarkar <105652044+rasarkar@users.noreply.github.com>
2024-07-18 11:10:40 -0400

    CONCD-868 "What can I work on" radio button bugs (#2458)
    * CONCD-868 trying to consolidate some code
    
    * CONCD-868 explicitly toggling radio button
    
    * CONCD-868 explicitly toggling radio button
    
    * CONCD-868 fixing a staticfiles error

---
+++ concordia/static/js/src/filter-assets.js
@@ -0,0 +1,12 @@
+/* global */
+/* exported filterAssets */
+function filterAssets(filterAssets, url) {
+    var button;
+    if (filterAssets) {
+        button = document.getElementById('show-all');
+    } else {
+        button = document.getElementById('filter-assets');
+    }
+    button.checked = false;
+    window.location = url;
+}
