63726dfd5aa6b79a27e1a944a93be890cd97f198
Rajit Sarkar <rsar@loc.gov>
2025-02-06 14:49:39 -0500

    CONCD-985 event listeners for mouse hover

--- concordia/static/js/src/homepage-carousel.js
+++ concordia/static/js/src/homepage-carousel.js
@@ -5,21 +5,38 @@ import {Carousel} from '/static/bootstrap/dist/js/bootstrap.esm.js';
 // initialization
 var carouselElement = document.getElementById('homepage-carousel');
 var carousel = new Carousel(carouselElement, {
-    interval: 5000,
+    interval: 1000,
     pause: false,
+    // pause: 'hover',
+    ride: 'carousel',
 });
 
 // play/pause
-document
-    .getElementById('play-pause-button')
-    .addEventListener('click', function () {
-        if ($(this).hasClass('paused')) {
-            carousel.cycle();
-            $(this).children('.fa').removeClass('fa-play').addClass('fa-pause');
-        } else {
-            carousel.pause();
-            // console.log(carousel);
-            $(this).children('.fa').removeClass('fa-pause').addClass('fa-play');
-        }
-        $(this).toggleClass('paused');
-    });
+
+let playPauseButton = document.getElementById('play-pause-button');
+playPauseButton.addEventListener('click', function () {
+    if ($(this).hasClass('paused')) {
+        carousel.cycle();
+    } else {
+        carousel.pause();
+        // console.log(carousel);
+    }
+    $(this).children('.fa').toggleClass('fa-pause').toggleClass('fa-play');
+    $(this).toggleClass('paused');
+});
+carouselElement.addEventListener('mouseenter', function () {
+    carousel.pause();
+});
+carouselElement.addEventListener('mouseover', function () {
+    carousel.pause();
+});
+carouselElement.addEventListener('mouseleave', function () {
+    if (!playPauseButton.classList.contains('paused')) {
+        carousel.cycle();
+    }
+});
+carouselElement.addEventListener('mouseout', function () {
+    if (!playPauseButton.classList.contains('paused')) {
+        carousel.cycle();
+    }
+});
