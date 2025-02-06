36fcbea8ad758e723e665a15bf20cd3a67d4da98
Rajit Sarkar <rsar@loc.gov>
2025-02-06 15:02:46 -0500

    CONCD-985 code cleanup

--- concordia/static/js/src/homepage-carousel.js
+++ concordia/static/js/src/homepage-carousel.js
@@ -5,9 +5,8 @@ import {Carousel} from '/static/bootstrap/dist/js/bootstrap.esm.js';
 // initialization
 var carouselElement = document.getElementById('homepage-carousel');
 var carousel = new Carousel(carouselElement, {
-    interval: 1000,
+    interval: 5000,
     pause: false,
-    // pause: 'hover',
     ride: 'carousel',
 });
 
@@ -19,23 +18,14 @@ playPauseButton.addEventListener('click', function () {
         carousel.cycle();
     } else {
         carousel.pause();
-        // console.log(carousel);
     }
     $(this).children('.fa').toggleClass('fa-pause').toggleClass('fa-play');
     $(this).toggleClass('paused');
-});
-carouselElement.addEventListener('mouseenter', function () {
-    carousel.pause();
 });
 carouselElement.addEventListener('mouseover', function () {
     carousel.pause();
 });
 carouselElement.addEventListener('mouseleave', function () {
-    if (!playPauseButton.classList.contains('paused')) {
-        carousel.cycle();
-    }
-});
-carouselElement.addEventListener('mouseout', function () {
     if (!playPauseButton.classList.contains('paused')) {
         carousel.cycle();
     }
