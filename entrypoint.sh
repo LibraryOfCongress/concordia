7698ca9d4c71302911964d551aea1b6820af9a18
joshuastegmaier <jstegmaier@loc.gov>
2025-03-10 17:17:13 +0000

    Removed commented-out code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,25 @@ set -e -u # Exit immediately for unhandled errors or undefined variables
 mkdir -p /app/logs
 touch /app/logs/concordia.log
 
-echo "Running makemigrations"
-./manage.py makemigrations --merge --noinput
+#echo "[$(date '+%Y-%m-%d %H:%M:%S')] Running makemigrations"
+#./manage.py makemigrations --merge --noinput
 
-echo "Running migrations"
-./manage.py migrate
+#echo "[$(date '+%Y-%m-%d %H:%M:%S')] Running migrations"
+#./manage.py migrate
 
-echo "Ensuring our base configuration is present in the database"
-./manage.py ensure_initial_site_configuration
+#echo "[$(date '+%Y-%m-%d %H:%M:%S')] Ensuring our base configuration is present in the database"
+#./manage.py ensure_initial_site_configuration
 
-if [ -v SENTRY_BACKEND_DSN ]; then
-    echo "Testing Sentry configuration"
-    echo "from sentry_sdk import capture_message;capture_message('This is a test event');" | ./manage.py shell
-fi
+#if [ -v SENTRY_BACKEND_DSN ]; then
+    #echo "[$(date '+%Y-%m-%d %H:%M:%S')] Testing Sentry configuration"
+    #echo "from sentry_sdk import capture_message;capture_message('This is a test event');" | ./manage.py shell
+#fi
 
-echo "Running collectstatic"
-./manage.py collectstatic --clear --noinput -v0
+#echo "[$(date '+%Y-%m-%d %H:%M:%S')] Running collectstatic"
+#./manage.py collectstatic --clear --noinput -v0
 
-echo "Running Django ASGI server"
-daphne -b 0.0.0.0 -p 80 concordia.asgi:application
+#echo "[$(date '+%Y-%m-%d %H:%M:%S')] Running Django ASGI server"
+#daphne -b 0.0.0.0 -p 80 concordia.asgi:application
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Launching bash shell"
+exec /bin/bash
