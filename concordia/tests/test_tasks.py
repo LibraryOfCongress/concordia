7542071426118e5e71afe6a053ba6421841b8f14
Rajit Sarkar <rsar@loc.gov>
2025-03-25 09:21:56 -0400

    CONCD-1128 don't disconnect signal handlers (without a comment explaining the particular reason)

--- concordia/tests/test_tasks.py
+++ concordia/tests/test_tasks.py
@@ -3,12 +3,10 @@ from unittest import mock
 
 from django.core import mail
 from django.core.cache import cache
-from django.db.models import signals
 from django.test import TestCase
 from django.utils import timezone
 
 from concordia.models import Campaign, SiteReport, Transcription
-from concordia.signals.handlers import on_transcription_save
 from concordia.tasks import (
     CacheLockedError,
     _daily_active_users,
@@ -46,7 +44,6 @@ class SiteReportTestCase(CreateTestUsers, TestCase):
         cls.item1 = cls.asset1.item
         cls.project1 = cls.item1.project
         cls.campaign1 = cls.project1.campaign
-        signals.post_save.disconnect(on_transcription_save, sender=Transcription)
         cls.asset1_transcription1 = create_transcription(
             asset=cls.asset1, user=cls.user1, accepted=timezone.now()
         )
