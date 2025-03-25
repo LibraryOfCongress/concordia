76bf295f39cffac1cd0c85be3911b92da8cbf680
joshuastegmaier <jstegmaier@loc.gov>
2025-03-25 14:55:52 +0000

    Updates to tests, test settings and test workflow to avoid issues with parallel tests

--- concordia/settings_test_jstegmaier.py
+++ concordia/settings_test_jstegmaier.py
@@ -5,6 +5,15 @@ DEBUG = False
 
 DATABASES["default"]["PORT"] = "5432"
 
+# Channel layers are used for different instances of an application
+# to communicate. We don't want that during tests, so we use
+# the in-memory layer to isolate it.
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
@@ -19,6 +28,10 @@ CACHES = {
         "LOCATION": "configuration-location",
     },
 }
+
+# These cause Celery to run tasks locally, synchronously and immediately
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
 
 DEFAULT_TO_EMAIL = "jstegmaier@loc.gov"
 
