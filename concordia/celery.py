4ea2e5513b70597cf86e1c808d050da43bb4d9c8
Josh Stegmaier <104993387+joshuastegmaier@users.noreply.github.com>
2025-04-09 06:42:29 -0400

    Reverted logging changes (main) (#2783)
    * Re-added inadvertently removed action (#2756)
    
    * Fixed asset images having an extraneous period (#2777)
    
    * Reverted changes to celery logging
    
    * Removed tests for logging changes that were reverted
    
    * Removed remaining test that used reverted changes

--- concordia/celery.py
+++ concordia/celery.py
@@ -4,7 +4,6 @@ import os
 
 import sentry_sdk
 from celery import Celery
-from celery.signals import setup_logging
 from sentry_sdk.integrations.celery import CeleryIntegration
 
 from concordia.version import get_concordia_version
@@ -27,18 +26,6 @@ app = Celery("concordia")
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
-
-# Celery won't be default use the logging settings
-# This configures Celery's logging to use our settings
-@setup_logging.connect
-def configure_logging(**kwargs):
-    import logging.config
-
-    from django.conf import settings
-
-    logging.config.dictConfig(settings.LOGGING)
-
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
