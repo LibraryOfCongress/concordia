4f0ecb5bd3226740304cf07b20143a19c577297a
Jen Kuenning <jkue@loc.gov>
2025-06-04 09:59:12 -0400

    updates to db_scripts to minimze attack surface and build explictly for the platform

--- db_scripts/dump/push-container.sh
+++ db_scripts/dump/push-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 619333082511.dkr.ecr.us-east-1.amazonaws.com
-docker build --no-cache --pull -t crowd-db-dump .
+docker build --platform linux/amd64 --no-cache --pull -t crowd-db-dump .
 docker tag crowd-db-dump:latest 619333082511.dkr.ecr.us-east-1.amazonaws.com/crowd-db-dump:latest
 docker push 619333082511.dkr.ecr.us-east-1.amazonaws.com/crowd-db-dump:latest
--- db_scripts/restore/push-container.sh
+++ db_scripts/restore/push-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 619333082511.dkr.ecr.us-east-1.amazonaws.com
-docker build --no-cache --pull -t crowd-db-restore .
+docker build --platform linux/amd64 --no-cache --pull -t crowd-db-restore .
 docker tag crowd-db-restore:latest 619333082511.dkr.ecr.us-east-1.amazonaws.com/crowd-db-restore:latest
 docker push 619333082511.dkr.ecr.us-east-1.amazonaws.com/crowd-db-restore:latest
