031e2dd9947c7e42410ea8571105be274ae23c59
joshuastegmaier <jstegmaier@loc.gov>
2025-06-05 13:39:21 +0000

    Separated asset status visualizations. Added templatetag to simplify adding visualizations to templates

--- concordia/static/js/src/visualizations/asset-status-by-campaign.js
+++ concordia/static/js/src/visualizations/asset-status-by-campaign.js
@@ -3,37 +3,6 @@ import {ConcordiaVisualization} from 'concordia-visualization';
 const colors = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'];
 
 document.addEventListener('DOMContentLoaded', () => {
-    // 1) Pie chart config: total assets by status
-    const assetStatusOverviewChart = new ConcordiaVisualization({
-        name: 'asset-status-overview',
-        canvasId: 'asset-status-overview',
-        chartType: 'pie',
-        title: 'Asset Status (All Active Campaigns)',
-        xLabel: '',
-        yLabel: '',
-        buildDataset: (payload) => {
-            return {
-                data: {
-                    labels: payload.status_labels,
-                    datasets: [
-                        {
-                            data: payload.total_counts,
-                            backgroundColor: colors,
-                        },
-                    ],
-                },
-                options: {
-                    scales: {
-                        // We don't want scales on a pie chart
-                        x: {display: false},
-                        y: {display: false},
-                    },
-                },
-            };
-        },
-    });
-
-    // 2) Stacked bar config: breakdown by campaign
     const assetStatusByCampaignChart = new ConcordiaVisualization({
         name: 'asset-status-by-campaign',
         canvasId: 'asset-status-by-campaign',
@@ -88,7 +57,5 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     });
 
-    // 3) Render both charts (each does its own fetch + error handling)
-    assetStatusOverviewChart.render();
     assetStatusByCampaignChart.render();
 });
--- concordia/static/js/src/visualizations/daily-activity.js
+++ concordia/static/js/src/visualizations/daily-activity.js
@@ -3,7 +3,7 @@ import {ConcordiaVisualization} from 'concordia-visualization';
 document.addEventListener('DOMContentLoaded', () => {
     const dailyActivityChart = new ConcordiaVisualization({
         name: 'daily-transcription-activity-by-campaign',
-        canvasId: 'daily-transcription-activity-by-campaign',
+        canvasId: 'daily-activity',
         chartType: 'bar',
         title: 'Daily Transcription Activity by Campaign (Last 7 Days)',
         xLabel: 'Date',
