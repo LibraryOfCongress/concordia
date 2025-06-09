227ee8a653e3cea6dc6e712716b1e2e6882b124b
Josh Stegmaier <104993387+joshuastegmaier@users.noreply.github.com>
2025-06-09 10:32:15 -0400

    Added basic set up for building a React app and running it on the /transcription path (#2888)
    * Added basic set up for building a React app and running it on the /transcription path
    
    * Fixed broken HTML in test template

--- concordia/urls.py
+++ concordia/urls.py
@@ -335,7 +335,16 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     from django.conf.urls.static import static
+    from django.views.generic import TemplateView
 
     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += (
+        path(
+            "transcription/",
+            TemplateView.as_view(template_name="transcriptions/transcription.html"),
+            name="transcription",
+        ),
+    )
