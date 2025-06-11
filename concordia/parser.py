91bbb67cea4a1008bca0adaa2546826bd320c01f
Rajit Sarkar <rsar@loc.gov>
2025-06-11 15:56:04 -0400

    CONCD-1221 use og:image (from the blog post's meta tags)

--- concordia/parser.py
+++ concordia/parser.py
@@ -1,21 +1,56 @@
+import html
+from html.parser import HTMLParser
+
 import defusedxml.ElementTree as ET
 import requests
 
 
-def fetch_blog_posts():
-    url = "https://blogs.loc.gov/thesignal/feed/"
-    response = requests.get(url, timeout=60)
+class OGImageParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.og_image = None
 
-    root = ET.fromstring(response.content)
-    channel = root.find("channel")
-    items = channel.findall("item")
-    feed_items = [
-        {
+    def handle_starttag(self, tag, attrs):
+        if tag.lower() == "meta":
+            attr_dict = dict(attrs)
+            print(attr_dict)
+            if attr_dict.get("property") == "og:image" and "content" in attr_dict:
+                self.og_image = attr_dict["content"]
+
+
+def extract_og_image(url):
+    """Fetch the meta value from the HTML."""
+    try:
+        response = requests.get(url, timeout=5)
+        parser = OGImageParser()
+        parser.feed(html.unescape(response.text))
+        return parser.og_image
+    except requests.RequestException:
+        return None
+
+
+def fetch_blog_posts():
+    """get and parse The Signal's RSS feed"""
+    try:
+        response = requests.get("https://blogs.loc.gov/thesignal/feed/", timeout=60)
+        response.raise_for_status()
+        root = ET.fromstring(response.content)
+    except Exception:
+        return []
+
+    items = root.find("channel").findall("item")
+    feed_items = []
+    for item in items[:18]:
+        feed_item = {
             "title": item.find("title").text,
-            "link": item.find("link").text,
             "description": item.find("description").text,
         }
-        for item in items[:18]
-    ]
+        link = item.find("link")
+        if link is not None:
+            feed_item["link"] = link.text
+            og_image = extract_og_image(link)
+            if og_image is not None:
+                feed_item["og:image"] = og_image
+        feed_items.append(feed_item)
 
     return feed_items
--- concordia/tests/test_parser.py
+++ concordia/tests/test_parser.py
@@ -1,8 +1,9 @@
 from unittest import mock
 
 from django.test import TestCase
+from requests.models import Response
 
-from concordia.parser import fetch_blog_posts
+from concordia.parser import extract_og_image, fetch_blog_posts
 
 TITLE = "What’s New Online at the Library of Congress: May 2025"
 LINK = "https://blogs.loc.gov/thesignal/2025/05/new-loc-may-2025/"
@@ -18,7 +19,6 @@ RSS = """<?xml version="1.0" encoding="UTF-8"?>
     </item>
     <item>
       <title>Volunteers Leverage OCR to Transcribe Library of Congress Digit...</title>
-      <link>https://blogs.loc.gov/thesignal/2025/05/volunteers-ocr/</link>
       <description>
         <![CDATA[Today’s guest post is from Lauren Algee, a Senior Digital Collec...]]>
       </description>
@@ -29,20 +29,43 @@ RSS = """<?xml version="1.0" encoding="UTF-8"?>
     LINK,
     DESCRIPTION,
 )
-
-
-def mocked_requests_get(*args, **kwargs):
-    class MockResponse:
-        content = RSS
-
-    return MockResponse()
+IMAGE = "https://blogs.loc.gov/thesignal/files/2025/05/loc-2017698702.png"
+HTML = (
+    """<html>
+  <head>
+    <meta property="og:image" content="%s"/>
+  </head>
+  <body></body>
+</html"""
+    % IMAGE
+)
 
 
 class ParserTestCase(TestCase):
-    @mock.patch("requests.get", side_effect=mocked_requests_get)
-    def test_fetch_blog_posts(self, mock_get):
-        items = fetch_blog_posts()
-        self.assertEqual(len(items), 2)
-        self.assertEqual(items[0]["title"], TITLE)
-        self.assertEqual(items[0]["link"], LINK)
-        self.assertEqual(items[0]["description"], DESCRIPTION)
+    @mock.patch("requests.get")
+    def test_extract_og_image(self, mock_urlopen):
+        mock_response = mock.MagicMock(spec=Response)
+        mock_response.text = HTML
+        mock_response.headers = {"Content-Type": "text/html"}
+        mock_urlopen.return_value = mock_response
+
+        image = extract_og_image("https://example.com/post1")
+        self.assertEqual(image, IMAGE)
+
+    @mock.patch("concordia.parser.extract_og_image")
+    @mock.patch("requests.get")
+    def test_fetch_blog_posts(self, mock_urlopen, mock_extract_og_image):
+        mock_response = mock.MagicMock(spec=Response)
+        mock_response.content = RSS
+        mock_response.status_code = 200
+        mock_urlopen.return_value = mock_response
+
+        mock_extract_og_image.return_value = IMAGE
+
+        feed_items = fetch_blog_posts()
+
+        self.assertEqual(len(feed_items), 2)
+        self.assertEqual(feed_items[0]["title"], TITLE)
+        self.assertEqual(feed_items[0]["link"], LINK)
+        self.assertEqual(feed_items[0]["description"], DESCRIPTION)
+        self.assertEqual(feed_items[0]["og:image"], IMAGE)
