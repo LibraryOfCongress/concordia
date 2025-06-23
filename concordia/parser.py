2768b417ecce546a68575641dd1510c1723447dc
Rajit Sarkar <rsar@loc.gov>
2025-06-23 15:59:35 -0400

    CONCD-1221 store results in cache

--- concordia/parser.py
+++ concordia/parser.py
@@ -3,6 +3,7 @@ from html.parser import HTMLParser
 
 import defusedxml.ElementTree as ET
 import requests
+from django.core.cache import cache
 
 
 class OGImageParser(HTMLParser):
@@ -20,10 +21,16 @@ class OGImageParser(HTMLParser):
 
 def extract_og_image(url):
     """Fetch the meta value from the HTML."""
+    cache_key = f"og_image:{url}"
+    cached_image = cache.get(cache_key)
+    if cached_image is not None:
+        return cached_image
+
     try:
         response = requests.get(url, timeout=5)
         parser = OGImageParser()
         parser.feed(html.unescape(response.text))
+        cache.set(cache_key, None, timeout=24 * 60 * 60)
         return parser.og_image
     except requests.RequestException:
         return None
