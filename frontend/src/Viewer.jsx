aa0f941cfcab506545800267d78b213217ebe6db
Josh Stegmaier <104993387+joshuastegmaier@users.noreply.github.com>
2025-07-08 13:04:07 -0400

    Add image viewer to React app (#2946)
    * Basic implementation of the image viewer in React
    
    * Broke viewer components out into separate React components
    
    * Added image filter functionality to React app
    
    * Removed custom API subclass because it didn't accomplish what it's supposed to

---
+++ frontend/src/Viewer.jsx
@@ -0,0 +1,94 @@
+import React, {useEffect, useRef} from 'react';
+import OpenSeadragon from 'openseadragon';
+import 'openseadragon-filtering';
+import screenfull from 'screenfull';
+import {prefixUrl, contactUrl} from './config.js';
+
+import ViewerControls from './ViewerControls';
+import ImageFilters from './ImageFilters';
+import KeyboardHelpModal from './KeyboardHelpModal';
+
+export default function Viewer({
+    imageUrl,
+    onLayoutHorizontal,
+    onLayoutVertical,
+}) {
+    const viewerRef = useRef(null);
+    const osdViewerRef = useRef(null);
+
+    useEffect(() => {
+        if (!viewerRef.current) return;
+
+        osdViewerRef.current = OpenSeadragon({
+            element: viewerRef.current,
+            prefixUrl: prefixUrl,
+            tileSources: {
+                type: 'image',
+                url: `${imageUrl}?canvas`,
+            },
+            gestureSettingsTouch: {
+                pinchRotate: true,
+            },
+            showNavigator: true,
+            showRotationControl: true,
+            showFlipControl: true,
+            zoomInButton: 'viewer-zoom-in',
+            zoomOutButton: 'viewer-zoom-out',
+            homeButton: 'viewer-home',
+            rotateLeftButton: 'viewer-rotate-left',
+            rotateRightButton: 'viewer-rotate-right',
+            flipButton: 'viewer-flip',
+            crossOriginPolicy: 'Anonymous',
+            drawer: 'canvas',
+            defaultZoomLevel: 0,
+            homeFillsView: false,
+        });
+
+        osdViewerRef.current.addHandler('open', () => {
+            setTimeout(() => {
+                osdViewerRef.current.viewport.goHome(true);
+            }, 0);
+        });
+
+        osdViewerRef.current.addHandler('open-failed', () => {
+            console.error('Unable to display image');
+            alert(`Unable to display image. Contact us at ${contactUrl}`);
+        });
+
+        return () => {
+            if (osdViewerRef.current) {
+                osdViewerRef.current.destroy();
+            }
+        };
+    }, [imageUrl]);
+
+    const toggleFullscreen = (e) => {
+        e.preventDefault();
+        if (!screenfull.isEnabled) return;
+        if (screenfull.isFullscreen) {
+            screenfull.exit();
+        } else {
+            screenfull.request(viewerRef.current);
+        }
+    };
+
+    return (
+        <div
+            id="viewer-column"
+            className="ps-0 d-flex align-items-stretch bg-dark d-print-block flex-column"
+        >
+            <ViewerControls
+                onLayoutHorizontal={onLayoutHorizontal}
+                onLayoutVertical={onLayoutVertical}
+                toggleFullscreen={toggleFullscreen}
+            />
+            <ImageFilters osdViewerRef={osdViewerRef} />
+            <KeyboardHelpModal />
+            <div
+                id="asset-image"
+                ref={viewerRef}
+                className="h-100 bg-dark d-print-none w-100"
+            ></div>
+        </div>
+    );
+}
