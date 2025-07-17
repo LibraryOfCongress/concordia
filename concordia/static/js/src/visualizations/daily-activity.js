412a1541f38c2a1811957acb35d3955fb377ec11
joshuastegmaier <jstegmaier@loc.gov>
2025-07-17 16:52:16 +0000

    Changed daily activity visualization to just show total by date for the last 28 days

--- concordia/static/js/src/visualizations/daily-activity.js
+++ concordia/static/js/src/visualizations/daily-activity.js
@@ -2,10 +2,10 @@ import {ConcordiaVisualization} from 'concordia-visualization';
 
 document.addEventListener('DOMContentLoaded', () => {
     const dailyActivityChart = new ConcordiaVisualization({
-        name: 'daily-transcription-activity-by-campaign',
+        name: 'daily-transcription-activity-last-28-days',
         canvasId: 'daily-activity',
         chartType: 'bar',
-        title: 'Daily Transcription Activity by Campaign (Last 7 Days)',
+        title: 'Daily Transcription Activity (Last 28 Days)',
         xLabel: 'Date',
         yLabel: 'Transcriptions + Reviews',
         buildDataset: (payload) => {
@@ -15,6 +15,23 @@ document.addEventListener('DOMContentLoaded', () => {
                     datasets: payload.transcription_datasets,
                 },
                 options: {
+                    scales: {
+                        x: {
+                            stacked: true,
+                            ticks: {
+                                callback: function (value, index) {
+                                    // Show only every 4th tick starting at index 3 (i.e., the 4th day)
+                                    return (index - 3) % 4 === 0
+                                        ? this.getLabelForValue(index)
+                                        : '';
+                                },
+                                autoSkip: false,
+                            },
+                        },
+                        y: {
+                            stacked: true,
+                        },
+                    },
                     plugins: {
                         legend: {
                             display: false,
