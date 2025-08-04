ff363151f354ed22c484b40a0733795b3e77e9e2
Jen Kuenning <jkue@loc.gov>
2025-08-04 11:26:28 -0400

    adding basic instrumentation and configuration for aws xray sdk

--- importer/entrypoint.sh
+++ importer/entrypoint.sh
@@ -5,5 +5,8 @@ set -e -u # Exit immediately for unhandled errors or undefined variables
 mkdir -p /app/logs
 touch /app/logs/concordia.log
 
+#  To avoid trace and reporting of errors in the X-Ray SDK
+export AWS_XRAY_CONTEXT_MISSING=LOG_ERROR
+
 echo "Running celery worker"
 celery -A concordia worker -l info -c 10
