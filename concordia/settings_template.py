3bac89a514e4522c75ecd18090806f86cb2febc5
Jen Kuenning <jkue@loc.gov>
2025-08-05 13:25:00 -0400

    more recorder order tweaks

--- concordia/settings_template.py
+++ concordia/settings_template.py
@@ -162,6 +162,20 @@ if is_web_process():
     INSTALLED_APPS += ["aws_xray_sdk.ext.django"]
     MIDDLEWARE += ["aws_xray_sdk.ext.django.middleware.XRayMiddleware"]
     XRAY_RECORDER = {
+        "AWS_XRAY_DAEMON_ADDRESS": os.environ.get(
+            "AWS_XRAY_DAEMON_ADDRESS", "127.0.0.1:2000"
+        ),
+        "AUTO_INSTRUMENT": True,
+        "AWS_XRAY_CONTEXT_MISSING": os.environ.get(
+            "AWS_XRAY_CONTEXT_MISSING", "LOG_ERROR"
+        ),
+        "PLUGINS": ("ECSPlugin"),
+        "AWS_XRAY_TRACING_NAME": os.environ.get(
+            "AWS_XRAY_TRACING_NAME",
+            os.environ.get("CONCORDIA_ENVIRONMENT", "development"),
+        ),
+        "PATCH_MODULES": ["boto3", "botocore", "requests", "httplib", "psycopg2"],
+        "SAMPLING": False,
         "IGNORE_MODULE_PATTERNS": [
             r"^django\.contrib\.admin\.views\.decorators\.cache",
             r"^django\.contrib\.admin\.options",
@@ -181,17 +195,6 @@ if is_web_process():
             r"^django\.contrib\.admin\.options\.ModelAdminDecoratorBase",
             r"^django\.contrib\.admin\.options\.InlineModelAdminDecoratorBase",
         ],
-        "AUTO_INSTRUMENT": True,
-        "AWS_XRAY_DAEMON_ADDRESS": os.environ.get(
-            "AWS_XRAY_DAEMON_ADDRESS", "127.0.0.1:2000"
-        ),
-        "AWS_XRAY_TRACING_NAME": os.environ.get(
-            "AWS_XRAY_TRACING_NAME",
-            os.environ.get("CONCORDIA_ENVIRONMENT", "development"),
-        ),
-        "PLUGINS": ("ECSPlugin"),
-        "PATCH_MODULES": ["boto3", "botocore", "requests", "httplib", "psycopg2"],
-        "SAMPLING": False,
     }
 
 
