a5701dd47a34b693a19646cb0288feb4ddb38c4d
Rajit Sarkar <rsar@loc.gov>
2025-08-22 15:44:44 -0400

    CONCD-1326 forgot to add the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import {defineConfig} from 'vite';
-// import path from 'path';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
     // root: '.',
@@ -8,14 +12,21 @@ export default defineConfig({
         outDir: 'static',
         emptyOutDir: false,
         rollupOptions: {
-            input: './concordia/static/js/src/main.js',
+            // input: path.resolve(__dirname, 'concordia/static/js/src/main.js'),
             output: {
                 entryFileNames: 'bundle.js',
+                // chunkFileNames: '[name].js';
                 assetFileNames: ({name}) =>
                     name && name.endsWith('.css')
                         ? 'css/bundle.css'
                         : '[name][extname]',
             },
+        },
+    },
+    resolve: {
+        alias: {
+            // '@js': path.resolve(__dirname, 'concordia/static/js/src'),
+            '@scss': path.resolve(__dirname, 'concordia/static/scss'),
         },
     },
 });
