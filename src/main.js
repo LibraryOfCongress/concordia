5ed868bc96ea973780d5ed180b4bbe13e1816949
Rajit Sarkar <rsar@loc.gov>
2025-09-11 09:18:22 -0400

    CONCD-1326 pushing current version of vite config (WiP)

--- src/main.js
+++ src/main.js
@@ -5,14 +5,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@scss/base.scss';
 
 /* local scripts */
-import '../concordia/static/js/src/asset-reservation.js';
-import '../concordia/static/js/src/guide.js';
+import '../concordia/static/js/src/banner.js';
 import '../concordia/static/js/src/homepage-carousel.js';
-import '../concordia/static/js/src/ocr.js';
-import {setTutorialHeight} from '../concordia/static/js/src/modules/quick-tips.js';
-import '../concordia/static/js/src/quick-tips-setup.js';
-import '../concordia/static/js/src/viewer.js';
-import '../concordia/static/js/src/viewer-split.js';
-import '../concordia/static/js/src/contribute.js';
-
-window.setTutorialHeight = setTutorialHeight;
