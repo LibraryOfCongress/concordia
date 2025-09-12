b0f4dc1c813bf2a0b47c2b0d8eec5f39527e4fd1
Rajit Sarkar <rsar@loc.gov>
2025-09-12 14:48:16 -0400

    CONCD-1326 might make more sense to bundle everything in to a single file

--- src/static-page.js
+++
@@ -1,3 +0,0 @@
-import '../concordia/static/js/src/about-accordions.js';
-import '../concordia/static/js/src/modules/concordia-visualization.js';
-import '../concordia/static/js/src/visualizations/daily-activity.js';
