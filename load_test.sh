aa988563a5c2c9ba81946a5ae652c81c4b7effea
joshuastegmaier <jstegmaier@loc.gov>
2025-09-22 19:28:48 +0000

    Added error messages when no work to be done during load test

--- load_test.sh
+++ load_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-locust  --headless -u 100 -r 2 --run-time 1m30s --host http://c2vldjsteg01.loctest.gov/
+locust  --headless -u 100 -r 2 --run-time 1m30s --host http://c2vldjsteg01.loctest.gov
